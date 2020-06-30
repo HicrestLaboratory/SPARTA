ae7c99294fba1d4f92c48f163ad65ac3d9856628
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2020-06-30 20:12:20 +0200

    Update sparse_utilities.cpp

--- src/general/sparse_utilities.cpp
+++ src/general/sparse_utilities.cpp
@@ -1427,7 +1427,7 @@ int main()
 
     int arr[5] = { 0,4,9,14,19 };
     int pattern[8] = { 0 };
-    int partition[9] = { 0,1,4,7,15,16,18,19,25 };
+    int partition[9] = { 0,1,4,7,15,16,17,20,25,40 };
     int mode = 1;
     int h = hash(arr, 5, partition, 0);
     get_pattern(arr, 5, partition, pattern, 0);
