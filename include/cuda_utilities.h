12bb2e58e7ac6793680be8d0f990acdc1f943879
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2020-07-05 20:08:46 +0200

    Update cuda_utilities.h

--- include/cuda_utilities.h
+++ include/cuda_utilities.h
@@ -4,4 +4,6 @@ void cublas_blockmat_multiply(const VBS& vbmatA, float* B, int B_cols, int B_lea
 
 int cublas_gemm_custom(const float* A, unsigned int A_rows, unsigned int A_cols, unsigned int lda, const float* B, unsigned int B_cols, unsigned int ldb, float* C, unsigned int ldc, const float alpha, const float beta);
 
+int cusparse_gemm_custom(const CSR& cmat, float* B, int B_cols, int B_lead_dim, float* C, int C_lead_dim, const float alpha, const float beta);
+
 
