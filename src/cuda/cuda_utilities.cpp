097a92a2015340e1d23cb38479891a9e3eff55c8
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2020-07-05 13:44:54 +0200

    Update cuda_utilities.cpp

--- src/cuda/cuda_utilities.cpp
+++ src/cuda/cuda_utilities.cpp
@@ -11,13 +11,12 @@
 #include <helper_cuda.h>
 
 #include<stdio.h>
+#include <iostream>
+
 
 #include "cuda_utilities.h"
 #include "comp_mats.h"
 #include "sparse_utilities.h"
-
-#include <iostream>
-#include "..\..\include\cuda_utilities.h"
 
 void cublas_blockmat_multiply(const VBS &vbmatA, float *B, int B_cols, int B_lead_dim, float *C, int C_lead_dim){
     //multiplies a VBS matrix (vbmatA) and a dense matrix (B); stores into (C)
