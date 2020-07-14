b16af11aa9c5335c64f15f6beae2eef553f46f95
fvella <flavio.vella@unibz.it>
2020-07-14 13:03:40 +0000

    fix script

--- script/small-test.sh
+++ script/small-test.sh
@@ -3,13 +3,13 @@
 OPTS="-w 1 -r 10 -v -1 -i 4"
 
 
-Mshapes=(1024 16384);
-Nshapes=(1024 16384);
-Kshapes=(1024 16384);
+Mshapes=(1024 4096);
+Nshapes=(1024 4096);
+Kshapes=(1024 4096);
 
 Pvalue=(256 512);
 Bvalue=(0.1 0.5 0.9);
-Qvalue=(0.001 0.05 0.1 0.2 0.4 0.8);
+Qvalue=(0.05 0.1 0.4 0.8);
 
 for m in ${Mshapes[@]}; do
   for k in ${Kshapes[@]}; do
@@ -18,7 +18,8 @@ for m in ${Mshapes[@]}; do
       for p in ${Pvalue[@]}; do
         for b in ${Bvalue[@]}; do
           for q in ${Qvalue[@]}; do
-            ./cuda_test $OPTS -m $m -n $n -k $k -p $p -b $b -q $q
+            ./cuda_test $OPTS -m $m -n $n -k $k -p $p -b $b -q $q sleep 0.5;
+          done
         done
       done
     done
