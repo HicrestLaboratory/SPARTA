8f4c92f71d90554de984f1eeedd7c605c59fefab
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2020-07-21 11:20:27 +0200

    Update small-test.sh

--- script/small-test.sh
+++ script/small-test.sh
@@ -2,9 +2,7 @@
 
 OPTS="-w 1 -r 3 -v -1 -i 4"
 
-Output_file="test_small_cublas_results.txt"
-
-echo "MIAO" > ${Output_file}
+: > test_small_cublas_results.txt
 
 Mshapes=(1024 2048);
 Nshapes=(1024 2048);
@@ -21,7 +19,7 @@ for m in ${Mshapes[@]}; do
       for p in ${Pvalue[@]}; do
         for b in ${Bvalue[@]}; do
           for q in ${Qvalue[@]}; do
-            ./programs/cuda/test_cublas_VBS ${OPTS} -m $m -n $n -k $k -p $p -b $b -q $q; sleep 0.5; 1> ${Output_file}
+            ./programs/cuda/test_cublas_VBS $OPTS -m $m -n $n -k $k -p $p -b $b -q $q; sleep 0.5; 1 >> test_small_cublas_results.txt
           done
         done
       done
