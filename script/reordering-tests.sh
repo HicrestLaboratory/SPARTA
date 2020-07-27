7c10b102e33610dc7ae7633413143f75fdb37cdd
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2020-07-27 16:51:00 +0200

    Update reordering-tests.sh

--- script/reordering-tests.sh
+++ script/reordering-tests.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
-OPTS="-w 1 -r 10 -v -1 -i 4"
+OPTS="-r 5 -v -1 -i 4"
 
 : > "test_reordering.txt"
 
-Mshapes=(1024 2048 4096 8192);
-Nshapes=(1024 2048 4096 8192 );
+m_shapes=(1024 2048 4096 8192);
+n_shapes=(1024 2048 4096 8192);
 
-Pvalue=(128 256 512 1024);
-pvalue=(128 256 512 1024);
-Bvalue=(0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0);
-Qvalue=(0.001 0.01 0.05 0.1 0.15 0.20 0.25 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0);
+P_value=(32 35 50 64 100 256 512);
+p_value=(64 128 256 512 1024);
+b_value=(0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0);
+q_value=(0.001 0.01 0.05 0.1 0.15 0.20 0.25 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0);
+e_value= (0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.99);
 
-for m in ${Mshapes[@]}; do
-  for k in ${Kshapes[@]}; do
-    for n in ${Nshapes[@]}; do
-#      #echo "$m $k $n"; sleep 0.2 
-      for p in ${Pvalue[@]}; do
-        for b in ${Bvalue[@]}; do
-          for q in ${Qvalue[@]}; do
-            	./programs/general/test_AHA $OPTS -m $m -n $n -k $k -p $p -b $b -q $q >> "test_reordering.txt"
-          done
+for m in ${m_shapes[@]}; do
+  for n in ${n_shapes[@]}; do
+    for e in ${e_value[@]}; do
+      for p in ${p_value[@]}; do
+	for p in ${P_value[@]}; do
+          for b in ${b_value[@]}; do
+            for q in ${q_value[@]}; do
+              ./programs/general/test_AHA $OPTS -m $m -n $n -e $e -p $p -P $P -b $b -q $q >> "test_reordering.txt"
+            done
+	  done
         done
       done
     done
