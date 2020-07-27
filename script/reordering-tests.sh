6a74626a9e772095ce8b9c3ac99c7c22dffebac7
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2020-07-27 16:41:21 +0200

    Create reordering-tests.sh

---
+++ script/reordering-tests.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+OPTS="-w 1 -r 10 -v -1 -i 4"
+
+: > "test_reordering.txt"
+
+Mshapes=(1024 2048 4096 8192);
+Nshapes=(1024 2048 4096 8192 );
+
+Pvalue=(128 256 512 1024);
+pvalue=(128 256 512 1024);
+Bvalue=(0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0);
+Qvalue=(0.001 0.01 0.05 0.1 0.15 0.20 0.25 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0);
+
+for m in ${Mshapes[@]}; do
+  for k in ${Kshapes[@]}; do
+    for n in ${Nshapes[@]}; do
+#      #echo "$m $k $n"; sleep 0.2 
+      for p in ${Pvalue[@]}; do
+        for b in ${Bvalue[@]}; do
+          for q in ${Qvalue[@]}; do
+            	./programs/general/test_AHA $OPTS -m $m -n $n -k $k -p $p -b $b -q $q >> "test_reordering.txt"
+          done
+        done
+      done
+    done
+  done
+done
+
+
+
