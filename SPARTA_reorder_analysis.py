aa32cc0309c288e9d808a17bb2a9ed62e24cfed2
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2020-08-30 12:51:49 +0200

    experiments testing

--- SPARTA_reorder_analysis.py
+++ SPARTA_reorder_analysis.py
@@ -8,6 +8,53 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 
+def check_fixed_condition(val, condition):
+    if type(condition) is list:
+        return (val >= condition[0] and val <= condition[1]);
+    else:
+        return val == condition;
+    
+def check_good_sample(i):
+    return (experiments["total_nonzeros"][i] != 0);
+
+
+def plot_x_vs_y(x_field_function, y_field_function, fixed, label = None, draw_std_error = True):
+    y_lists = {};
+    y_values = [];
+    y_errors =[];
+    for i in range(n_exp):
+        #check condition for adding experiment
+        skip = False; 
+        for fixed_field, condition in fixed.items():
+            if not check_fixed_condition(experiments[fixed_field][i], condition):
+                skip = True;
+            if not check_good_sample:
+                skip = True;
+        if skip:
+            continue;   
+        
+        x = x_field_function(i);
+        if x not in y_lists:
+            y_lists[x] = [];
+        
+        y = y_field_function(i);
+        
+        #append to data list
+        y_lists[x].append(y);
+        
+    for x in y_lists.keys():
+        y_values.append(np.mean(y_lists[x]));
+        y_errors.append(np.std(y_lists[x]));
+
+    x_values = list(y_lists.keys());
+    plt.scatter(x_values,y_values, label = label);
+    
+    if draw_std_error:
+        plt.fill_between(x_values, np.array(y_values) - np.array(y_errors),
+                             np.array(y_values) + np.array(y_errors), alpha=0.1,
+                             color="r");
+
+
 
 saving_path = "images/reorder_experiment";
 try:
@@ -23,30 +70,53 @@ with open(datasetName, 'r') as f:
     fields = fields.split();
     for elem in fields:
         experiments[elem] = [];
+    experiments["scrambled"] = [];
 
-    while (True):
-        line = f.readline();
-        if not line:
-            break;
-        line = line.split();
-        if (line[0] == "input_type"):
-            continue;
-        for elem in zip(fields, line):
-            experiments[elem[0]].append(float(elem[1]));
+    stop = False;
+    while ( not stop ):
+        for scrambled in [0,1]:
+            line = f.readline();
+            if not line:
+                stop = True;
+                break
+            line = line.split();
+            if (line[0] == "input_type"):
+                continue;
+            if (scrambled == 0):
+                for elem in zip(fields, line):
+                    experiments[elem[0]].append(float(elem[1]));
+                experiments["scrambled"].append(scrambled); #s is missed in the output report, so added here
+            line = f.readline();
+       
+        
+print(fields)
+def npslice(var_name,eps,algo_bs):
+    return np.array(experiments[var_name])[i_vals[eps][algo_bs]];            
             
-            
+print("IMPORT DONE")
             
 n_exp = len(experiments["input_type"]);
 
-
-def npslice(var_name,eps,algo_bs):
-    return np.array(experiments[var_name])[i_vals[eps][algo_bs]];
-
 input_block_size = 64;
-input_block_density = 0.5;
+input_block_density = 0.01;
 i_vals = {};
 
 
+plt.figure();
+x_field_function = lambda i: experiments["input_entries_density"][i]*experiments["input_blocks_density"][i];
+y_field_function = lambda i: experiments["total_nonzeros"][i]/experiments["VBS_total_nonzeros"][i];
+for algo_block_size in [100,]:
+    fixed = {"input_blocks_density": 0.9, "input_block_size": 64, "algo_block_size": algo_block_size, "epsilon" : 0.99};
+    plot_x_vs_y(x_field_function, y_field_function, fixed, label = algo_block_size);
+
+plt.xlabel("input density");
+plt.ylabel("output in-block density");
+plt.legend()
+plt.show()
+
+
+
+plt.figure()
 for i in range(n_exp):
     if experiments["total_nonzeros"][i] != 0 and\
     experiments["input_blocks_density"][i] == input_block_density and\
@@ -90,7 +160,7 @@ for eps in i_vals:
 
 
 plt.figure();
-algo_bs = 50;
+algo_bs = 11;
 title = "algorithm block size = " + str(algo_bs) +\
     "\n input block size = " + str(input_block_size) +\
     "\n input density of nonzero blocks = " + str(input_block_density);
@@ -115,8 +185,8 @@ plt.savefig(saving_path + '/Idensity_vs_Odensity_varying_epsilon.jpg', format = 
 
 input_block_size = 64;
 input_entries_density = 0.2 ;
-ibd = [0.1, 0.2, 0.4]
-algo_block_size = 35;
+ibd = [0.01, 0.2, 0.4]
+algo_block_size = 37;
 
 #    "\n input density of nonzero blocks = " + str(input_block_density) +\
 
