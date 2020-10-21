00f01bc680463aa6e80e4ef4249aa65d884f3af2
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2020-10-21 17:43:25 +0200

    Update reordering-test-real.sh

--- script/reordering-test-real.sh
+++ script/reordering-test-real.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-​  
-for f in "../"*
+​ 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DATADIR = "$(DIR)/../data"
+
+for f in "$(DATADIR)"/*
 do
   echo $(basename $f)
 done
