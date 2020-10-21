f3ebc38f32cd13935c7d3c13a3e1023fc53aca23
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2020-10-21 17:45:22 +0200

    Update reordering-test-real.sh

--- script/reordering-test-real.sh
+++ script/reordering-test-real.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-DATADIR="$(DIR)/../data"
+echo "The script you are running has basename `basename "$0"`, dirname `dirname "$0"`"
 
-for f in "$(DATADIR)"/*
+for f in "../"*
 do
   echo $(basename $f)
 done
