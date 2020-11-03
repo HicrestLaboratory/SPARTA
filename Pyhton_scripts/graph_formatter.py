0466df60c1db4f55be68c91872f5d64aa95e92c0
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2020-11-03 19:12:53 +0100

    changes to experiments

--- Pyhton_scripts/graph_formatter.py
+++ Pyhton_scripts/graph_formatter.py
@@ -4,8 +4,30 @@ Created on Tue Nov  3 12:59:00 2020
 
 @author: Paolo
 """
-
 import os
+
+def find_separator(selected_doc):
+    with open(selected_doc, 'r') as f:
+        line = "#"
+        print(line[0])
+        while not (line[0].isnumeric()):
+            line = f.readline();
+        i = 0
+        while((line[i]).isnumeric() or line[i] == '.'):
+            i += 1;
+        return line[i];
+
+
+def change_separator(selected_doc, new_doc, old_separator, new_separator):
+     with open(selected_doc, 'r') as f:
+        with open(new_doc, 'w') as write_f:
+            for line in f:
+                line = f.readline();
+                if (not (line[0]).isnumeric()):
+                    continue
+                splitline = line.split(old_separator);
+                newline = splitline[0] + new_separator + splitline[1] + "\n";
+                write_f.write(newline);
 
 os.chdir("../data/")
 documents = os.listdir()
@@ -22,30 +44,15 @@ while True:
     selected_idx = int(selected_idx);
     selected_doc = documents[selected_idx]
     
-    with open(selected_doc, 'r') as f:
-        line = "#"
-        print(line[0])
-        while not (line[0].isnumeric()):
-            line = f.readline();
-        i = 0
-        while((line[i]).isnumeric() or line[i] == '.'):
-            i += 1;
-        separator = line[i];
-        
+    separator = find_separator(selected_doc);
     print("separator: ", repr(separator))
+    
     new_separator = input("Insert new separator:")
     if (new_separator == "\n"):
         print("exiting")
         exit()
     
-    with open(selected_doc, 'r') as f:
-        with open("fixed_" + selected_doc, 'w') as write_f:
-            for line in f:
-                line = f.readline();
-                if (not (line[0]).isnumeric()):
-                    continue
-                splitline = line.split(separator);
-                newline = splitline[0] + new_separator + splitline[1] + "\n";
-                write_f.write(newline);
-            
+    change_separator(selected_doc, "fixed_" + selected_doc, separator, new_separator)
     print("new file created")
+    
+    
---
+++ script/synthetic-performance-ultrasparse.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+OPTS="-w 1 -r 10 -v -1 -i 4"
+
+: > "test_complete_cublas_results_v2.txt"
+
+Mshapes=(1024 2048 4096 8192 16384);
+Nshapes=(1024 2048 4096 8192 16384);
+Kshapes=(1024 2048 4096 8192 16384);
+
+Pvalue=(32 64 128 256 512 1024);
+Bvalue=(0.05 0.1 0.15 0.2 0.25 0.3 0.4);
+Qvalue=(0.0001 0.0002 0.0005 0.001 0.002 0.005 0.01 0.05 0.1);
+
+for m in ${Mshapes[@]}; do
+  for k in ${Kshapes[@]}; do
+    for n in ${Nshapes[@]}; do
+#      #echo "$m $k $n"; sleep 0.2 
+      for p in ${Pvalue[@]}; do
+        for b in ${Bvalue[@]}; do
+          for q in ${Qvalue[@]}; do
+            	./programs/cuda/test_cublas_VBS $OPTS -m $m -n $n -k $k -p $p -b $b -q $q >> "test_complete_cublas_results.txt"
+          done
+        done
+      done
+    done
+  done
+done
+
+
+
