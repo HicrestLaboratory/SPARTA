00af63d42af5776e1132d960b7bb55f0ae8ebf9b
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2020-11-03 11:30:44 +0100

    Update reordering-test-real.sh

--- script/reordering-test-real.sh
+++ script/reordering-test-real.sh
@@ -6,13 +6,14 @@ P_value=(5 8 12 16 32 64 128 256);
 e_value=(0.5 0.6 0.8 0.9 0.99);
 s_value=(1 3);
 ​
+: > "real_reordering_results.txt"
+
 for f in "data/"*; do
-	: > "results_" + $f
 	for e in ${e_value[@]}; do
 		for P in ${P_value[@]}; do
 	      		for s in ${s_value[@]}; do
 				echo $f $e $P $s
-	          		./programs/general/test_AHA $OPTS -e $e -P $P -s $s -f $f >> "results_" + $f
+	          		./programs/general/test_AHA $OPTS -e $e -P $P -s $s -f $f >> "real_reordering_results.txt"
 			done
 		done
 	done
