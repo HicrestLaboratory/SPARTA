77a7dc81a8ba4934e6c98bd019f61fd175e20cff
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2020-11-05 13:25:06 +0100

    Update sparse_utilities.cpp

--- src/general/sparse_utilities.cpp
+++ src/general/sparse_utilities.cpp
@@ -697,6 +697,9 @@ int convert_to_VBS(const CSR& cmat, VBS& vbmat, intT block_rows, intT* row_part,
     }
 
 
+    //DEBUG
+    arr_print(row_part, block_rows);
+    arr_print(col_part, col_rows);
 
     //bookmarks nonzero blocks
     for (intT i = 0; i < cmat_main_dim; i++)
