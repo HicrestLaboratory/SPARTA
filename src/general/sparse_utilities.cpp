9a2ff6a2ab211328c515a24270fccd4a4362ec2b
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2020-11-05 15:11:32 +0100

    Update sparse_utilities.cpp

--- src/general/sparse_utilities.cpp
+++ src/general/sparse_utilities.cpp
@@ -726,8 +726,6 @@ int convert_to_VBS(const CSR& cmat, VBS& vbmat, intT block_rows, intT* row_part,
             current_block_col--;
 
             //flag the bookmark position (nonzero block)
-            std::cout << "entry in position " << row << " " << col << std::endl;
-            std::cout << "found nonzero block in position " << *current_main_pos << " " << *current_second_pos << std::endl;
             blocks_bookmark[*current_main_pos][*current_second_pos] = -2;
         }
 
@@ -748,10 +746,12 @@ int convert_to_VBS(const CSR& cmat, VBS& vbmat, intT block_rows, intT* row_part,
             intT block_col = vbmat.blocks_fmt == 0 ? jb : ib;
             if (blocks_bookmark[ib][jb] == -2)
             {
+                std::cout << "nonzero block in " << block_row << " " << block_col << std::endl;
                 blocks_bookmark[ib][jb] = total_area;
                 total_area += (row_part[block_row + 1] - row_part[block_row]) * (col_part[block_col + 1] - col_part[block_col]);
                 vbmat.nzcount[ib] += 1;
                 total_nz_blocks += 1;
+                std::cout << "total_area " << total_area << "nzcount[" << ib << "] = " << vbmat.nzcount[ib] << std::endl;
 
             }
         }
