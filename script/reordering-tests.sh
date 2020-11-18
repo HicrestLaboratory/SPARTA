163d698b0645f08236eab29e24746a37a6deac62
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2020-11-18 19:06:33 +0100

    several changes

--- script/reordering-tests.sh
+++ script/reordering-tests.sh
@@ -2,16 +2,16 @@
 
 OPTS="-r 3 -v -1 -i 4"
 
-: > "test_reordering.txt"
+: > "test_reordering_synth.txt"
 
 m_shapes=(1024 2048);
 n_shapes=(1024 2048);
 
-P_value=(5 11 23 37 41 64 100);
-p_value=(8 16 32 64 128);
+P_value=(23 41 64 128 200 256);
+p_value=(8 16 32 64 128 200);
 b_value=(0.01 0.1 0.2 0.3 0.5 0.7 0.9);
 q_value=(0.01 0.02 0.05 0.1 0.3 0.5 0.8 1.0);
-e_value=(0.5 0.6 0.8 0.9 0.99);
+e_value=(0.5 0.6 0.65 0.7 0.75 0.8);
 s_value=(1 3);
 
 for m in ${m_shapes[@]}; do
@@ -23,7 +23,7 @@ for m in ${m_shapes[@]}; do
             for q in ${q_value[@]}; do
 	      for s in ${s_value[@]}; do
 		  echo $m $n $e $p $P $b $q
-	          ./programs/general/test_AHA $OPTS -m $m -n $n -e $e -p $p -P $P -b $b -q $q >> "test_reordering.txt"
+	          ./programs/general/test_AHA $OPTS -m $m -n $n -e $e -p $p -P $P -b $b -q $q >> "test_reordering_synth.txt"
               done
             done
 	  done
