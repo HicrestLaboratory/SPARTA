3dd58bcc9875663cb974bd31aee780f14cba4265
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2021-02-08 14:29:04 +0100

    Update test_ncVBS.cpp

--- test/general/test_ncVBS.cpp
+++ test/general/test_ncVBS.cpp
@@ -147,4 +147,16 @@ int main(int argc, char* argv[]) {
     }
 
 
+    ncVBS vbmat;
+    int mat_rows = 10;
+    int mat_cols = 20;
+    int block_size = 5;
+    float mat_density = 0.1f;
+    float row_density = 0.2f;
+
+    random_ncVBS(vbmat, mat_rows, mat_cols, block_size, mat_density, row_density)
+
+
+
+
 }
