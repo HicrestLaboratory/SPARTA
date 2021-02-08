5af4bbfe5ee597dadf7800775619ddb61fa72e74
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2021-02-08 14:30:49 +0100

    minor changes

--- include/nvbs_utilities.h
+++ include/nvbs_utilities.h
@@ -6,7 +6,7 @@ int cleanVBS(ncVBS& vbmat);
 
 int initialize_ncVBS(ncVBS& vbmat, intT mat_rows, intT block_cols, intT* col_part);
 
-int random_ncVBS(ncVBS& vbmat, intT mat_rows, intT mat_cols, intT block_size, float mat_density, float row_density, int mode, int block_variation, float density_variation);
+int random_ncVBS(ncVBS& vbmat, intT mat_rows, intT mat_cols, intT block_size, float mat_density, float row_density, int mode = 0, int block_variation = 0, float density_variation = 0);
 
 int convert_to_mat(const ncVBS& vbmat, DataT* out_mat, int out_mat_fmt);
 
