d0d2caff6b6e6fe4eb40d6ed670d78e6c0d73197
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2021-02-10 18:02:43 +0100

    Update cuda_utilities_ncVBS.cpp

--- src/cuda/cuda_utilities_ncVBS.cpp
+++ src/cuda/cuda_utilities_ncVBS.cpp
@@ -29,7 +29,7 @@
 */
 
 
-void cublas_ncVBS_multiply(ncVBS& vbmatA, const DataT* B, int B_cols, int B_lead_dim, DataT* C, int C_lead_dim, float& dt, int n_streams_mult = 32, int n_streams_cpy = 32)
+void cublas_ncVBS_multiply(ncVBS& vbmatA, const DataT* B, int B_cols, int B_lead_dim, DataT* C, int C_lead_dim, float& dt, int n_streams_mult, int n_streams_cpy)
 {
     cudaDataType_t cuda_type;
     if (typeid(DataT) == typeid(float))    cuda_type = CUDA_R_32F;
