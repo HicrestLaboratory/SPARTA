9f3d12f7af6e6756d73e75dab6c724ea5ddeebfd
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2021-02-10 18:01:48 +0100

    Update cuda_utilities_ncVBS.h

--- include/cuda_utilities_ncVBS.h
+++ include/cuda_utilities_ncVBS.h
@@ -1,3 +1,3 @@
 #pragma once
 
-void cublas_ncVBS_multiply(ncVBS& vbmatA, const DataT* B, int B_cols, int B_lead_dim, DataT* C, int C_lead_dim, float& dt, int n_streams_mult, int n_streams_cpy);
+void cublas_ncVBS_multiply(ncVBS& vbmatA, const DataT* B, int B_cols, int B_lead_dim, DataT* C, int C_lead_dim, float& dt, int n_streams_mult = 32, int n_streams_cpy = 32);
