3486fc901bd1d4951fa9393b74ddac8cd0cf930c
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2021-02-10 18:00:11 +0100

    test for cuda ncvbs

--- test/cuda/test_cublas_ncVBS.cpp
+++ test/cuda/test_cublas_ncVBS.cpp
@@ -81,6 +81,13 @@ int main(int argc, char* argv[]) {
 
 	std::cout << "\n EQUALITY CHECK: MULTIPLICATION: " << equal(C_rows, C_cols, mat_C, C_cols, 0, mat_C2, C_cols, 0, 0.00001f) << std::endl;
 
+
+	DataT* mat_C3 = new DataT[C_rows * C_cols]{ 0 };
+	float dt = 0;
+	cublas_ncVBS_multiply(vbmat, mat_B, B_cols, B_cols, mat_C3, C_cols, dt);
+
+
+
 }
  
  
