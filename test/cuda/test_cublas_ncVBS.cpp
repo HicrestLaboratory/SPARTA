be663abae5877fb4910f07af05647e47a254c3f8
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2021-02-11 16:09:21 +0100

    Update test_cublas_ncVBS.cpp

--- test/cuda/test_cublas_ncVBS.cpp
+++ test/cuda/test_cublas_ncVBS.cpp
@@ -42,6 +42,8 @@ int main(int argc, char* argv[]) {
 	float mat_density = 0.2f;
 	float row_density = 0.2f;
 	float A_sparsity = 0.2f;
+	float B_sparsity = 0.9f;
+
 
     opterr = 0;
     char c;
@@ -68,60 +70,78 @@ int main(int argc, char* argv[]) {
         }
 
 
+	int C_rows = A_rows;
+	int C_cols = B_cols;
 
-
-
+	//CREATING A RANDOM MATRIX (A)
 	DataT* mat_A = new DataT[A_rows * A_cols]{ 0 };
-
 	random_mat(mat_A, A_rows, A_cols, A_sparsity);
-
 	std::cout << "\n A" << std::endl;
 	//matprint(mat_A, A_rows, A_cols, A_cols, 0);
 
-
+	//CONVERTING TO VBMAT
 	std::cout << "\n VBMAT_A" << std::endl;
 	intT block_cols = A_cols / block_size;
 	intT* col_part = new intT[block_cols + 1];
 	partition(col_part, 0, A_cols, block_size);
 	convert_to_ncVBS(mat_A, A_rows, A_cols, 0, A_cols, vbmat, block_cols, col_part);
 	//matprint(vbmat);
-	std::cout << "\n EQUALITY CHECK: vbmat and mat: " << equal(vbmat, mat_A, A_rows, A_cols, A_cols, 0) << std::endl;
 
-
+	//CREATING A RANDOM MATRIX (B)
 	int B_rows = A_cols;
 	DataT* mat_B = new DataT[B_rows * B_cols]{ 0 };
-
-	random_mat(mat_B, B_rows, B_cols, A_sparsity);
-
-	std::cout << "\n B" << std::endl;
+	random_mat(mat_B, B_rows, B_cols, B_sparsity);
+	std::cout << "\n B created" << std::endl;
 	//matprint(mat_B, B_rows, B_cols, B_cols, 0);
 
-	int C_rows = A_rows;
-	int C_cols = B_cols;
+
+	//MULTIPLYING SERIAL
 	DataT* mat_C = new DataT[C_rows * C_cols]{ 0 };
 	multiply(mat_A, A_rows, A_cols, 0, A_cols, mat_B, B_cols, 0, B_cols, mat_C, C_cols, 0);
-
 	std::cout << "\n C" << std::endl;
 	//matprint(mat_C, C_rows, C_cols, C_cols, 0);
 
-	std::cout << "\n C multiplied with vbmat" << std::endl;
+
+	//MULTIPLYING WITH VBMAT
+	std::cout << "\n multiplying with vbmat" << std::endl;
 	DataT* mat_C2 = new DataT[C_rows * C_cols]{ 0 };
 	multiply(vbmat, mat_B, B_cols, 0, B_cols, mat_C2, C_cols, 0);
 	//matprint(mat_C2, C_rows, C_cols, C_cols, 0);
+	std::cout << "EQUALITY CHECK: MULTIPLICATION: " << equal(C_rows, C_cols, mat_C, C_cols, 0, mat_C2, C_cols, 0, 0.00001f) << std::endl;
 
-	std::cout << "\n EQUALITY CHECK: MULTIPLICATION: " << equal(C_rows, C_cols, mat_C, C_cols, 0, mat_C2, C_cols, 0, 0.00001f) << std::endl;
 
-	std::cout << "\n C multiplied with vbmat CUBLAS" << std::endl;
-	DataT* mat_C3 = new DataT[C_rows * C_cols]{ 0 };
+	//MULTIPLYING WITH VBMAT CUBLAS
+	std::cout << "\n multiplying with vbmat CUBLAS" << std::endl;
+	DataT* mat_C_vbs_cublas = new DataT[C_rows * C_cols]{ 0 };
 	float* dt = new float[6];
-	cublas_ncVBS_multiply(vbmat, mat_B, B_cols, B_cols, mat_C3, C_cols, dt);
+	cublas_ncVBS_multiply(vbmat, mat_B, B_cols, B_cols, mat_C_vbs_cublas, C_cols, dt);
 	//matprint(mat_C3, C_rows, C_cols, C_cols, 0);
 
-
-	//TODO more detailed timing
-	std::cout << "\n EQUALITY CHECK: MULTIPLICATION: " << equal(C_rows, C_cols, mat_C, C_cols, 0, mat_C3, C_cols, 0, 0.00001f) << std::endl;
+	std::cout << "EQUALITY CHECK: MULTIPLICATION: " << equal(C_rows, C_cols, mat_C, C_cols, 0, mat_C_vbs_cublas, C_cols, 0, 0.00001f) << std::endl;
 	std::cout << "TIME MEASUREMENTS:" << std::endl;
-	for (int i = 0; i < 6;  i++) std::cout << dt[i] << " " << std::endl;
+	for (int i = 0; i < 6; i++) std::cout << dt[i] << " ";
+	std::cout << std::endl;
+
+	//MULTIPLYING WITH CUSPARSE
+
+	//TODO do not start with array but create directly the CSR?
+
+	CSR cmat_A;
+	convert_to_CSR(mat_A, A_rows, A_cols, 0, cmat_A, 0);
+	//prepare the cusparse CSR format
+	int nnz = count_nnz(cmat_A);
+	int* csrRowPtr = new int[cmat_A.rows + 1];
+	int* csrColInd = new int[nnz];
+	float* csrVal = new float[nnz];
+	prepare_cusparse_CSR(cmat_A, csrRowPtr, csrColInd, csrVal);
+
+	float dt;
+	DataT* mat_C_cusparse = new DataT[C_rows * C_cols]{ 0 };
+	std::cout << "\n multiplying with CUSPARSE" << std::endl;
+	cusparse_gemm_custom(cmat_A.rows, cmat_A.cols, nnz, csrRowPtr, csrColInd, csrVal, mat_B, B_cols, B_rows, mat_C_cusparse, C_rows, 1.0f, 0.0f, dt);
+	std::cout << "EQUALITY CHECK: MULTIPLICATION: " << equal(C_rows, C_cols, mat_C, C_cols, 0, mat_C_cusparse, C_cols, 0, 0.00001f) << std::endl;
+	std::cout << "TIME MEASUREMENT:" << dt << std::endl;
+
 
 }
  
