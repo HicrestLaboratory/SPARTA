bac76cde47c8d7a1529f7a73041f67bbf377a952
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2021-02-15 15:41:27 +0100

    testing

--- script/reordering-test-real.sh
+++ script/reordering-test-real.sh
@@ -6,10 +6,10 @@ P_value=(64 128 200 256 300 512);
 e_value=(0.5 0.6 0.65 0.7 0.75 0.8 );
 s_value=(1 3);
 ​
-RESULTS = "results/real_reordering_results_v2.txt"
+RESULTS = "results/real_reordering_results.txt"
 : > $(RESULTS)
 
-for f in "data/"*; do
+for f in "data/real_graphs/"*; do
 	for e in ${e_value[@]}; do
 		for P in ${P_value[@]}; do
 	      		for s in ${s_value[@]}; do
