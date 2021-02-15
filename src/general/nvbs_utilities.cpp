40a691fe0b99ae3749e67cea89c07be2ad5915cf
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2021-02-15 17:25:29 +0100

    Update nvbs_utilities.cpp

--- src/general/nvbs_utilities.cpp
+++ src/general/nvbs_utilities.cpp
@@ -324,13 +324,9 @@ int convert_to_ncVBS(const CSR& cmat, ncVBS& vbmat, intT block_cols, intT* col_p
         intT nz = 0;
         while(nz < n_elems)
         {
-            std::cout << "i:" << i << " nz: " << nz << " j: " << cmat.ja[i][nz] << std::endl;
-
             DataT elem = cmat.ma[i][nz];
             while (cmat.ja[i][nz] >= col_part[block + 1]) block++; //find in which block the nz is;
             vbmat.nzcount[block]++; //flag this row as nonzero;
-
-            std::cout << "block" << col_part[block + 1] << std::endl;
 
             intT width = vbmat.block_width(block);
             nzindices[block].push_back(i); //store the row index;
