3c185d9e6ecb1b870de5de83f7bbcc8ec21c6427
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2021-02-15 15:55:06 +0100

    Update reordering-test-real.sh

--- script/reordering-test-real.sh
+++ script/reordering-test-real.sh
@@ -6,7 +6,7 @@ P_value=(64 128 200 256 300 512);
 e_value=(0.5 0.6 0.65 0.7 0.75 0.8 );
 s_value=(1 3);
 ​
-RESULTS = "results/real_reordering_results.txt"
+RESULTS="results/real_reordering_results.txt";
 
 for f in "data/*"; do
 	for e in ${e_value[@]}; do
