3c52fcd05dc251c1aa213798d4abafc2d9d7bc32
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2021-02-15 17:32:03 +0100

    Update test_ncVBS.cpp

--- test/general/test_ncVBS.cpp
+++ test/general/test_ncVBS.cpp
@@ -21,11 +21,11 @@ int main(int argc, char* argv[]) {
 
     int A_rows = 10;
     int A_cols = 20;
-    int B_cols = 5;
+    int B_cols = 6;
     int block_size = 5;
     float mat_density = 0.5f;
     float row_density = 0.3f;
-    float A_sparsity = 0.3f;
+    float A_sparsity = 0.1f;
     intT block_cols = A_cols / block_size;
     intT* col_part = new intT[block_cols + 1];
     partition(col_part, 0, A_cols, block_size);
