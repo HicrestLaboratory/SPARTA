c4ed686d31471528ab125c1dbc02a38fcb66b552
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2021-02-16 16:57:19 +0100

    Update compression-test-vs.sh

--- script/compression-test-vs.sh
+++ script/compression-test-vs.sh
@@ -1,19 +1,19 @@
-#!/bin/bash
+#!binbash
 
-OPTS="-i 2 -v -1"
+OPTS=-i 2 -v -1
 
 P_value=(64 128 200 256 300 512);
 e_value=(0.7 0.8 0.9);
 s_value=(1 3);
 ​
-RESULTS="results/compression_vs_results.txt";
+RESULTS=resultscompression_vs_results.txt;
 
-for f in "data/*"; do
+for f in data; do
 	for e in ${e_value[@]}; do
 		for P in ${P_value[@]}; do
 	      		for s in ${s_value[@]}; do
 				echo $f $e $P $s
-	          		./programs/general/test_M1vsM2 $OPTS -e $e -P $P -s $s -f $f >> $(RESULTS)
+	          		.programsgeneraltest_M1vsM2 $OPTS -e $e -P $P -s $s -f $f  $(RESULTS)
 			done
 		done
 	done
