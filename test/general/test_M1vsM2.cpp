ad48cb7a3ae5b354ee4f08f7299225c758f52b7e
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2021-02-16 11:36:00 +0100

    Update test_M1vsM2.cpp

--- test/general/test_M1vsM2.cpp
+++ test/general/test_M1vsM2.cpp
@@ -402,7 +402,7 @@ int main(int argc, char* argv[]) {
     svi nc_nz_per_block;
     intT nc_nz_blocks = 0;
 
-    std::cout << algo_col_blocks << std::endl;
+    std::cout << algo_block_cols << std::endl;
     arr_print(algo_col_part, algo_block_cols);
     convert_to_ncVBS(input_cmat, vbmat, algo_block_cols, algo_col_part);
 
