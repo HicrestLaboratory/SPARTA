9170221267af4b8601cef206a4929f1264813a1a
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2021-05-06 16:41:52 +0200

    experiments on saad

---
+++ Pyhton_scripts/SPARTA_reorder_analysis_real_v2.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  5 12:04:46 2021
+
+@author: Paolo
+"""
+
+
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+
+def change_sep(string, newsep, oldsep = " "):
+    string = string.split(oldsep);
+    newstring = "";
+    for elem in string:
+        newstring += elem + newsep
+    return newstring[:-1];
+
+def convert_to_CSV(filename, new_file_name):
+    with open(filename, 'r') as infile:
+        with open(new_file_name, 'w') as outfile:
+            fieldsline = infile.readline();
+            
+            firstrow = change_sep(fieldsline, ",");
+            
+            outfile.writelines(firstrow);
+        
+            stop = False;
+            while ( not stop ):
+                    line = infile.readline();
+                    if not line:
+                        stop = True;
+                        break
+                    newline = change_sep(line, ",")
+                    outfile.writelines(newline);                    
+                    line = infile.readline();
+                    
+                    
+folder = "../results/";
+name = "reordering_vs_saad_fixed"
+filename = folder + name + ".txt";
+csv_filename = folder + name + ".csv";
+                    
+convert_to_CSV(filename, csv_filename);
+results_df = pd.read_csv(csv_filename);
+
+data_top = results_df.columns 
+
+columsn = ['exp_name', 
+           'input_type', 
+           'input_source', 
+           'reorder_algorithm', 
+           'rows',
+           'cols',
+           'total_nonzeros',
+           'input_blocks_density',
+           'input_entries_density',
+           'input_block_size',
+           'algo_block_size',
+           'epsilon',
+           'scramble',
+           'VBS_avg_nzblock_height',
+           'VBS_avg_nzblock_height_error', 
+           'VBS_total_nonzeros',
+           'VBS_total_nonzeros_error', 
+           'VBS_block_rows', 
+           'VBS_block_rows_error',
+           'VBS_nz_blocks', 
+           'VBS_nz_blocks_error', 
+           'VBS_min_block_H',
+           'VBS_min_block_H_error', 
+           'VBS_max_block_H', 
+           'VBS_max_block_H_error']
+
+results_df["density"] = results_df.apply(lambda x: x['total_nonzeros']/(x['rows']*x["cols"]), axis=1)
+results_df["VBS_density"] = results_df.apply(lambda x: x['VBS_total_nonzeros']/(x['rows']*x["cols"]), axis=1)
+
+
+source = "data/real_graphs/CA-CondMat.txt";
+
+plt.figure()
+
+scramble = 3;
+algo_block_size = 64;
+rows = 60341;
+
+ax = plt.gca();
+y = "VBS_avg_nzblock_height";
+x = "epsilon"
+q = 'rows == @rows and algo_block_size == @algo_block_size and scramble == @scramble and reorder_algorithm == "saad"' 
+results_df.query(q).plot(x = x, y = y, kind = "scatter", ax = ax);
+
+
+
+q = 'rows == @rows and algo_block_size == @algo_block_size and scramble == @scramble and reorder_algorithm == "saad_blocks"' 
+results_df.query(q).plot(x = x, y = y, kind = "scatter", color = "red", ax = ax);
--- script/reordering-test-real-vs-saad.sh
+++ script/reordering-test-real-vs-saad.sh
@@ -5,7 +5,7 @@ RESULTS="results/reordering_vs_saad.txt";
 >"${RESULTS}";
 
 P_value=(16 32 64 128 200 256 300 512);
-e_value=(0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9);
+e_value=(0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9);
 s_value=(1 3);
 
 for f in data/real-graphs/*.txt; do
