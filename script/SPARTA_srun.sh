a5a2c06a66a5f78c4f0d1de5dbde5f4f6e5fd60d
Sylos Labini Paolo (Student Com20) <pasyloslabini@slurm-ctrl.inf.unibz.it>
2021-08-17 11:27:37 +0200

    minor changes to the SPARTA run script

--- script/SPARTA_srun.sh
+++ script/SPARTA_srun.sh
@@ -8,7 +8,7 @@
 #SBATCH --gres=gpu
 #SBATCH --nodes 1
 #SBATCH --time=99:00:00
-
+#SBATCH --mem-per-cpu=32G
 #SBATCH --ntasks=1
 
 
@@ -19,4 +19,4 @@ cd /home/clusterusers/pasyloslabini/SPARTA
 make clean
 make test_cublas_VBS
 cd /home/clusterusers/pasyloslabini/SPARTA
-./programs/cuda/test_cublas_VBS ${OPTS} -m 32000 -k 32000 -n 1024 -p 64
+./programs/cuda/test_cublas_VBS ${OPTS} -m 320000 -n 128 -k 320000 -p 128 -b 0.01 -q 0.06
