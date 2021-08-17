6ff233651ebb62f2d8d09ba947731025a0181102
Sylos Labini Paolo (Student Com20) <pasyloslabini@slurm-ctrl.inf.unibz.it>
2021-08-17 11:20:19 +0200

    added SPARTA_script_launch to launch general script on the unibz cluster

---
+++ script/SPARTA_script_launch.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH --job-name=sparta-test
+#SBATCH --output=sparta-test-%j.out
+#SBATCH --error=sparta-test-%j.err
+
+#SBATCH --partition training
+#SBATCH --gres=gpu
+#SBATCH --nodes 1
+#SBATCH --time=99:00:00
+
+#SBATCH --ntasks=1
+
+
+export OPTS="-w 1 -r 5 -v -1 -i 4"
+module load cuda-10.2
+module load gcc-9.3.0
+cd /home/clusterusers/pasyloslabini/SPARTA
+make clean
+make test_cublas_cusparse_comparison
+cd /home/clusterusers/pasyloslabini/SPARTA
+source ${1}  #call any script from here
