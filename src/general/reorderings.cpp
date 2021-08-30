bb80750f937a47950c86e8d51a01bf852e7d3f19
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2021-08-30 12:43:21 +0200

    Update reorderings.cpp

--- src/general/reorderings.cpp
+++ src/general/reorderings.cpp
@@ -686,6 +686,8 @@ int make_group_structure(intT* &group_structure, intT &group_structure_nzcount, 
         group_structure = new intT[len_A];
         group_structure_nzcount = len_A;
         std::copy(cols_A, cols_A + len_A, group_structure);
+        std::cout << "making the structure" << std::endl;
+        arr_print(group_structure, group_structure_nzcount);
     }
     else if (params.reorder_algo == "saad_blocks")
     {
