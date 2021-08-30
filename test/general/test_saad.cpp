b1d99c36a4dfdf7b5f4e1420f70671d22a14709a
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2021-08-30 13:14:29 +0200

    Update test_saad.cpp

--- test/general/test_saad.cpp
+++ test/general/test_saad.cpp
@@ -61,7 +61,6 @@ int main(int argc, char* argv[]) {
         scramble_input(input_cmat, params);
 
 
-        cout << "scrambled" << endl;
         int vbmat_blocks_fmt = 0;
         int vbmat_entries_fmt = 0;
         intT algo_block_cols = std::ceil((float)params.A_cols / params.algo_block_size);
@@ -70,6 +69,7 @@ int main(int argc, char* argv[]) {
         intT* algo_col_part = new intT[algo_block_cols + 1]; 
         partition(algo_col_part, 0, params.A_cols, params.algo_block_size);
 
+        cout << "starting reordering" << endl;
         //run the reordering algo
         intT* hash_groups = new intT[params.A_rows];
         saad_reordering(input_cmat, params, hash_groups);
