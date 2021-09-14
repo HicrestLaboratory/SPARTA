ec195f2cb96050db490dec510d7ce535dc4140f7
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2021-09-14 12:06:45 +0200

    Update SPARTA_performance_analysis_pandas.py

--- Pyhton_scripts/SPARTA_performance_analysis_pandas.py
+++ Pyhton_scripts/SPARTA_performance_analysis_pandas.py
@@ -9,41 +9,56 @@ Created on Tue Apr  6 11:16:49 2021
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
-
-def change_sep(string, newsep, oldsep = " "):
-    string = string.split(oldsep);
-    newstring = "";
-    for elem in string:
-        newstring += elem + newsep
-    return newstring[:-1];
+import argparse
 
 def convert_to_CSV(filename, new_file_name):
     with open(filename, 'r') as infile:
         with open(new_file_name, 'w') as outfile:
             fieldsline = infile.readline();
             
-            firstrow = change_sep(fieldsline, ",");
+            firstrow = ",".join(fieldsline.split(" "))
             
             outfile.writelines(firstrow);
         
             stop = False;
+            count = 0;
             while ( not stop ):
                     line = infile.readline();
                     if not line:
                         stop = True;
                         break
-                    newline = change_sep(line, ",")
+                    newline = ",".join(line.split(" "))
                     outfile.writelines(newline);                    
                     line = infile.readline();
+                    count+= 1;
+            print("read ", count, " lines");
                     
                     
-folder = "../results/";
-name = "test_complete_cublas_results_ultrasparse_5"
-filename = folder + name + ".txt";
-csv_filename = folder + name + ".csv";
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description="Makes images from experiments")
+    
+    parser.add_argument("--input-file", default="/",
+        help="file that contains the already run experiments")
+    parser.add_argument("--output-csv", default = "",
+        help="file of the new csv to output")
+    parser.add_argument("--convert-to-csv", default = 0,
+        help="convert input file to csv if 1. input must be already a csv if 0")
+    parser.add_argument("--output-dir", default="/",
+        help="directory where the images are saved")
+
+    args = parser.parse_args()
+
+    input_file = args.input_file;
+    output_csv = args.output_csv;
+    convert_to_csv = int(args.convert_to_csv);
+    output_dir = args.output_dir;
                     
-convert_to_CSV(filename, csv_filename);
-results_df = pd.read_csv(csv_filename);
+if (convert_to_csv): convert_to_CSV(input_file, output_csv);
+results_df = pd.read_csv(output_csv);
+
+exit;
 
 columns = ['input_type',
  'A_rows',
