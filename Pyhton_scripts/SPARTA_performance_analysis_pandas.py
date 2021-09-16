5643778cd33b66a88d43280dbfffec135406c264
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2021-09-16 17:20:28 +0200

    Update SPARTA_performance_analysis_pandas.py

--- Pyhton_scripts/SPARTA_performance_analysis_pandas.py
+++ Pyhton_scripts/SPARTA_performance_analysis_pandas.py
@@ -89,7 +89,7 @@ print(results_df["input_blocks_density"].unique())
 
 
 results_df["winner"] = results_df.apply(winner, axis = 1);
-q = "cols == 1024 and rows == 1024 and B_cols == 4096 and input_block_size == 64"
+q = "cols == 2048 and rows == 2048 and B_cols == 8192 and input_block_size == 64"
 results_df.query(q).plot(y = "input_blocks_density", x = "input_entries_density", kind = "scatter", c = "winner", colormap='viridis');
 
 
@@ -106,7 +106,7 @@ ax = plt.gca();
 
 #ax.set_yscale("log")
 ax.set_xscale("log")
-ax.set_xlim([0.0005, 0.3]);
+ax.set_xlim([0.0005, 0.2]);
 plt.show();
 
 
@@ -116,14 +116,14 @@ y = "sp_vs_cu";
 x = "input_entries_density"
 
 rows = 2048
-cols = 4096;
-B_cols = 4096;
+cols = 2048;
+B_cols = 8192;
 input_block_size = 64;
-input_blocks_density = 0.5
+input_blocks_density = 0.2
 
 q = "rows == {} and cols == {} and B_cols == {} and input_block_size == {} and input_blocks_density == {}".format(rows, cols, B_cols, input_block_size, input_blocks_density)
 results_df.query(q).plot(x = x, y = y, color = "red", ax = ax);
-ax.set(ylabel='speedup vs cusparse', xlabel='density of blocks',
+ax.set(ylabel='speedup vs cusparse', xlabel='density inside blocks',
        title='Comparison with cusparse \n N, K, M = {}, {}, {} \n block size = {} \n input_blocks_density = {}'.format(rows, cols, B_cols, input_block_size, input_blocks_density))
 ax.axhline(1)
 x = results_df.query(q)["input_entries_density"].tolist();
@@ -132,45 +132,13 @@ errors = np.array(results_df.query(q)["VBSmm_std"].tolist());
 upper = y + 3*errors;
 lower = y - 3*errors;
 
+ax.set_xscale("log")
+ax.set_xlim([0.0005, 0.2]);
+
 plt.grid(b=True, which='major', color='#666666', linestyle='-', alpha=0.4)
 plt.minorticks_on()
 plt.grid(b=True, which='minor', color='#999999', linestyle='-', alpha=0.2, linewidth=0.5)
          
 ax.fill_between(x, lower, upper, alpha=0.5, edgecolor='#CC4F1B', facecolor='#FF9848')
 plt.show()
-
-
-
-plt.figure()
-ax = plt.gca();
-y = "sp_vs_cu";
-x = "B_cols"
-
-cols = 1024;
-input_blocks_density = 0.15;
-input_blocks_density = 128;
-density = 0.5
-
-q = "cols == {} and input_blocks_density == {} and input_blocks_density == {} and density < {}".format(cols, input_blocks_density, input_blocks_density, density)
-results_df.query(q).plot(x = x, y = y, kind = "scatter", color = "red", ax = ax);
-ax.set(ylabel='speedup vs cusparse', xlabel='M',
-       title='Comparison with cusparse \n N, K,= 1024, {}, \n density of blocks = {} \n block size = {} \n global density = {}'.format(cols, input_blocks_density, input_blocks_density, density))
-ax.axhline(1)
-
-plt.figure()
-ax = plt.gca();
-y = "sp_vs_cu";
-x = "input_blocks_density"
-
-cols = 1024;
-B_cols = 16384;
-input_blocks_density = 64;
-density = 0.001
-
-q = "cols == {} and B_cols == {} and input_blocks_density == {} and density < {}".format(cols, B_cols, input_blocks_density, density)
-results_df.query(q).plot(x = x, y = y, kind = "scatter", color = "red", ax = ax);
-ax.set(ylabel='speedup vs cusparse', xlabel='density of blocks',
-       title='Comparison with cusparse \n N, K, M = 1024, {}, {} \n block size = {} \n global density = {}'.format(cols, B_cols, input_blocks_density, density))
-ax.axhline(1)
-
 
