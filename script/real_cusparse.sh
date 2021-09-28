0b58369d975ac690effbfbda54a6cedebb7e43db
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2021-09-28 11:43:26 +0200

    Update real_cusparse.sh

--- script/real_cusparse.sh
+++ script/real_cusparse.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-OPTS="-w 1 -r 10 -v -1 -i 2"
+OPTS="-w 1 -r 10 -v -1 -i 2 -a 5"
 
 RESULTS = "results/real_cusparse_results.txt"
-: > ${RESULTS}
-
-n_shapes=(1024 2048 4096 8192 16384);
-
+: > $(RESULTS)
 
 for f in "data/*"; do
-	for n in ${m_shapes[@]}; do
-            	./programs/cuda/test_cublas_VBS $OPTS -f $f -n $n>> ${RESULTS}
+            	./programs/cuda/test_cublas_VBS $OPTS -f $f >> $(RESULTS)
 done
 
 
