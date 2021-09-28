a382200dde4a2c78b66b6c21e1ef3f2d545b5dab
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2021-09-28 11:39:41 +0200

    added new test (real_cusparse)

---
+++ script/real_cusparse.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+OPTS="-w 1 -r 10 -v -1 -i 2"
+
+RESULTS = "results/real_cusparse_results.txt"
+: > ${RESULTS}
+
+n_shapes=(1024 2048 4096 8192 16384);
+
+
+for f in "data/*"; do
+	for n in ${m_shapes[@]}; do
+            	./programs/cuda/test_cublas_VBS $OPTS -f $f -n $n>> ${RESULTS}
+done
+
+
+
