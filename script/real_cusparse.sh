6693de544c49680cc7350ad6bd5fe281be8215b6
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2021-09-28 11:47:22 +0200

    Update real_cusparse.sh

--- script/real_cusparse.sh
+++ script/real_cusparse.sh
@@ -2,8 +2,8 @@
 
 OPTS="-w 1 -r 10 -v -1 -i 2 -a 5"
 
-RESULTS = "results/real_cusparse_results.txt"
-: > ${RESULTS}
+RESULTS="results/real_cusparse_results.txt"
+: >"${RESULTS}";
 
 n_shapes=(1024 2048 4096 8192 16384);
 
