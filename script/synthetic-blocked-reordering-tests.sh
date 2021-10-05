25bdf3438638680f86c19d967b48d9a009c8b46d
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2021-10-05 18:03:18 +0200

    Update synthetic-blocked-reordering-tests.sh

--- script/synthetic-blocked-reordering-tests.sh
+++ script/synthetic-blocked-reordering-tests.sh
@@ -10,8 +10,7 @@ k_shapes=(2048);
 
 e_value=(0.1 0.2 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9);
 
-P_value=(64);
-p_value=(64);
+P_value=(16 32 64);
 
 b_value=(0.01 0.1 0.2 0.3 0.4 0.5);
 q_value=(0.005 0.01 0.02 0.05 0.1 0.2 0.5);
@@ -22,20 +21,18 @@ s_value=(1);
 for m in ${m_shapes[@]}; do
   for k in ${k_shapes[@]}; do
     for e in ${e_value[@]}; do
-      for p in ${p_value[@]}; do
 	for P in ${P_value[@]}; do
           for b in ${b_value[@]}; do
             for q in ${q_value[@]}; do
 	      for s in ${s_value[@]}; do
 		for F in ${F_value[@]}; do
 		  echo $m $n $e $p $P $b $q
-	          ./programs/general/test_AHA $OPTS -m $m -k $k -e $e -p $p -P $P -b $b -q $q -F $F -s $s>> ${RESULTS}
+	          ./programs/general/test_AHA $OPTS -m $m -k $k -e $e -p $P -P $P -b $b -q $q -F $F -s $s>> ${RESULTS}
                 done
 	      done
             done
 	  done
         done
-      done
     done
   done
 done
