d78bf0388dd790cfad9f88ab1fdefaeaba2ab795
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2021-10-05 18:01:59 +0200

    new synthetic experiments (optimality)

--- script/synthetic-blocked-reordering-tests.sh
+++ script/synthetic-blocked-reordering-tests.sh
@@ -2,22 +2,22 @@
 
 OPTS="-r 3 -v -1 -i 4 -R saad-blocks -N ReorderingBlockedSynth"
 
-RESULTS = "results/test_reordering_blocked_synth.txt"
+RESULTS = "results/test_reordering_blocked_synth_2.txt"
 : > ${RESULTS};
 
-m_shapes=(1024 2048);
-k_shapes=(1024 2048);
+m_shapes=(2048);
+k_shapes=(2048);
 
-e_value=(0.5 0.6 0.65 0.7 0.75 0.8);
+e_value=(0.1 0.2 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9);
 
-P_value=(16 23 41 64 100 128);
-p_value=(16 32 64 128);
+P_value=(64);
+p_value=(64);
 
-b_value=(0.01 0.1 0.2 0.3 0.5 0.7 0.9);
-q_value=(0.005 0.01 0.02 0.05 0.1 0.3);
+b_value=(0.01 0.1 0.2 0.3 0.4 0.5);
+q_value=(0.005 0.01 0.02 0.05 0.1 0.2 0.5);
 
 F_value=(jaccard scalar);
-s_value=(1 3);
+s_value=(1);
 
 for m in ${m_shapes[@]}; do
   for k in ${k_shapes[@]}; do
