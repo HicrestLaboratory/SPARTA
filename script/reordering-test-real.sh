bd7902fc3bb8959ebdfb8ed634af7a470e260465
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2021-10-25 16:24:27 +0200

    Update reordering-test-real.sh

--- script/reordering-test-real.sh
+++ script/reordering-test-real.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-OPTS="-i 2 -v -1"
+OPTS="-i 2 -v -1 -R saad-blocks -s 1"
 
 P_value=(64 128 200 256 300 512);
-e_value=(0.5 0.6 0.65 0.7 0.75 0.8 );
-s_value=(1 3);
+e_value=(0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9);
+F_value=(scalar jaccard);
 ​
-RESULTS="results/real_reordering_results.txt";
+RESULTS="results/real_reordering_results_25_10.txt";
 
 for f in "data/*"; do
 	for e in ${e_value[@]}; do
 		for P in ${P_value[@]}; do
-	      		for s in ${s_value[@]}; do
-				echo $f $e $P $s
-	          		./programs/general/test_AHA ${OPTS} -e $e -P $P -s $s -f $f >> $(RESULTS)
+	      		for F in ${F_value[@]}; do
+				echo $f $e $P $F
+	          		./programs/general/test_saad ${OPTS} -e $e -P $P -F $F>> ${RESULTS}
 			done
 		done
 	done
