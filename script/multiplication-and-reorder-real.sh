a9b91c00ca4ab631b3ec0ae33ea896d0ec7bcd60
Sylos Labini Paolo (Student Com20) <pasyloslabini@slurm-ctrl.inf.unibz.it>
2021-11-26 16:12:14 +0100

    new real reordering and mult experiments

---
+++ script/multiplication-and-reorder-real.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+OPTS="-r 5 -v -1 -i 4 -R saad_blocks -s 1 -M 1 -a 1 -F jaccard"
+DATA={1}
+
+DATE=$(date +"%m-%d-%Y");
+RESULTS="results/test_cublas_reordering-${DATE}.txt"
+
+:>${RESULTS};
+
+m_shapes=(4096);
+k_shapes=(4096);
+n_shapes=(2048 4096 8192)
+
+e_value=(0.1 0.2 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9 0.95);
+
+P_value=(32 64 128);
+l_value=(0 1 2);
+
+for f in "${1}*"; do
+  for e in ${e_value[@]}; do
+    for P in ${P_value[@]}; do
+      for n in ${n_shapes[@]}; do
+        for l in ${l_value[@]}; do 
+	      echo $f $P
+	      ./programs/cuda/test_cublas_reorder ${OPTS} -f $f -n $n -e $e -P $P -l $l >> ${RESULTS}
+	done
+      done
+    done
+  done
+done
+
+
+
