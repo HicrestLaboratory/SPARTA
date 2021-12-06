e4513819fc5c97210a4f8efd4fa11ef7ba51aff0
Sylos Labini Paolo (Student Com20) <pasyloslabini@slurm-ctrl.inf.unibz.it>
2021-12-06 17:52:44 +0100

    fixed a typo

--- script/multiplication-and-reorder-real.sh
+++ script/multiplication-and-reorder-real.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-OPTS="-r 5 -v -1 -i 2 -R saad_blocks -s 1 -M 1 -a 1 -F jaccard"
+OPTS="-r 3 -v -1 -i 2 -R saad_blocks -s 1 -M 1 -a 1 -F jaccard"
 DATA={1}
 
 DATE=$(date +"%m-%d-%Y");
@@ -8,8 +8,6 @@ RESULTS="results/test_cublas_reordering-${DATE}.txt"
 
 :>${RESULTS};
 
-m_shapes=(4096);
-k_shapes=(4096);
 n_shapes=(2048 4096 8192)
 
 e_value=(0.1 0.2 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9 0.95);
@@ -17,7 +15,7 @@ e_value=(0.1 0.2 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9 0.95)
 P_value=(32 64 128);
 l_value=(0 1 2);
 
-for f in "${1}*"; do
+for f in ${1}*; do
   for e in ${e_value[@]}; do
     for P in ${P_value[@]}; do
       for n in ${n_shapes[@]}; do
