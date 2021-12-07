12a46baacf1e4ccc5d654be4d3f5ea3158971573
Sylos Labini Paolo (Student Com20) <pasyloslabini@slurm-ctrl.inf.unibz.it>
2021-12-07 19:03:23 +0100

    fixed the scrip

--- script/multiplication-and-reorder-real.sh
+++ script/multiplication-and-reorder-real.sh
@@ -4,7 +4,7 @@ OPTS="-r 10 -v -1 -i 2 -R saad_blocks -s 1 -M 1 -a -1 -F jaccard"
 DATA={1}
 
 DATE=$(date +"%m-%d-%Y");
-RESULTS="results/test_cublas_reordering-${DATE}.txt"
+RESULTS="results/test_cublas_reordering-real-${DATE}.txt"
 
 :>${RESULTS};
 
@@ -17,8 +17,8 @@ for f in ${1}*; do
   for e in ${e_value[@]}; do
     for P in ${P_value[@]}; do
         for l in ${l_value[@]}; do 
-	      echo $f $e $P $l date
-	      ./programs/cuda/test_cublas_reorder_optimized ${OPTS} -f $f -e $e -P $P -l $l >> ${RESULTS}
+		echo $f $e $P $l $(date)
+	      	./programs/cuda/test_cublas_reorder_optimized ${OPTS} -f $f -e $e -P $P -l $l >> ${RESULTS}
 	done
     done
   done
