7b7a06dcf6e5d82caec5a71af21dbeb6d8c3a19c
Sylos Labini Paolo (Student Com20) <pasyloslabini@slurm-ctrl.inf.unibz.it>
2021-12-07 15:52:34 +0100

    added date stamps to output

--- script/multiplication-and-reorder-real.sh
+++ script/multiplication-and-reorder-real.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-OPTS="-r 3 -v -1 -i 2 -R saad_blocks -s 1 -M 1 -a 1 -F jaccard"
+OPTS="-r 3 -v -1 -i 2 -R saad_blocks -s 1 -M 1 -a -1 -F jaccard"
 DATA={1}
 
 DATE=$(date +"%m-%d-%Y");
@@ -8,11 +8,11 @@ RESULTS="results/test_cublas_reordering-${DATE}.txt"
 
 :>${RESULTS};
 
-n_shapes=(2048 4096 8192)
+n_shapes=(4096 8192 32768 65536)
 
 e_value=(0.1 0.2 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9 0.95);
 
-P_value=(32 64 128);
+P_value=(64 128 256);
 l_value=(0 1 2);
 
 for f in ${1}*; do
@@ -20,7 +20,7 @@ for f in ${1}*; do
     for P in ${P_value[@]}; do
       for n in ${n_shapes[@]}; do
         for l in ${l_value[@]}; do 
-	      echo $f $P
+	      echo $f $P date
 	      ./programs/cuda/test_cublas_reorder ${OPTS} -f $f -n $n -e $e -P $P -l $l >> ${RESULTS}
 	done
       done
