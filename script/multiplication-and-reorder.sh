f20b9757211661728f2d5af1de3422402de34e75
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2022-01-10 23:12:07 +0100

    alpah

--- script/multiplication-and-reorder.sh
+++ script/multiplication-and-reorder.sh
@@ -10,16 +10,16 @@ RESULTS="results/test_cublas_reordering-synthetic-${DATE}.txt"
 
 m_shapes=(8192);
 k_shapes=(8192);
-n_shapes=(2048 4096 8192 16384)
+n_shapes=(2048 4096 8192)
 
 e_value=(0.01 0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9);
 
-P_value=(32 64 256);
+P_value=(64);
 
-b_value=(0.05 0.1 0.2 0.3 0.4 0.5);
+b_value=(0.01 0.05 0.1 0.2 0.3 0.4 0.5);
 q_value=(0.005 0.01 0.02 0.05 0.1 0.2 0.5);
 
-l_value=(-1 0 2 4);
+l_value=(-1 0);
 
 
 for m in ${m_shapes[@]}; do
