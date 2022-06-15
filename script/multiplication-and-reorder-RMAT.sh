50170a9988794670b1e79fac5a6e8aa810f29fa6
Sylos Labini Paolo (Student Com20) <pasyloslabini@slurm-ctrl.inf.unibz.it>
2022-06-15 17:31:15 +0200

    changed for submission

--- script/multiplication-and-reorder-RMAT.sh
+++ script/multiplication-and-reorder-RMAT.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-OPTS="-r 4 -v -1 -i 2 -R saad_blocks -s 1 -a -1 -F jaccard -M 1"
+OPTS="-r 3 -v -1 -i 2 -R saad_blocks -s 1 -a -1 -F jaccard -M 1"
 DATA={1}
 
 DATE=$(date +"%m-%d-%Y");
@@ -8,10 +8,10 @@ RESULTS="results/test_cublas_reordering-RMAT-${DATE}.txt"
 
 :>${RESULTS};
 
-e_value=(0.001 0.01 0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9);
+e_value=(0.1 0.5);
 
-P_value=(64 128 256);
-l_value=(-1 0);
+P_value=(64);
+l_value=(0);
 
 for f in ${1}*; do
   for e in ${e_value[@]}; do
