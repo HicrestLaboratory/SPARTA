fd1c3635c9c54eba436c4bf1160cfda1a9fe805e
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2022-07-21 16:10:12 +0200

    read_edgelist added

--- src/matrices.cpp
+++ src/matrices.cpp
@@ -1,25 +1,32 @@
 #pragma once
 
 #include "matrices"
+#include <string>
+#include <vector>
+#include <iostream>
 
 using namespace std;
+
+void CSR::print(ofstream& outfile)
+{
+}
 
 void CSR::reorder(vector<intT> permutation)
 {
 }
 
-void CSR::read_from_edgelist(ifstream& infile, string delimiter, bool pattern_only)
+void CSR::read_from_edgelist(ifstream& infile, string delimiter = "\t", bool pattern_only = true)
 {
+
     intT last_node = -1;
     intT current_node;
-    std::string temp;
+    string temp;
+    vector<vector<intT>> pos_holder;
+    vector<vector<DataT>> val_holder;
     intT max_column = 0;
-    vector<intT> pos_holder;
-    vector<DataT> val_holder;
     intT i = -1; 
     
     while (infile.peek() == '#' or infile.peek() == '%') infile.ignore(2048, '\n');
-    //while (new node equal last):
     while (getline(infile, temp)) {
 
         int del_pos = temp.find(delimiter);
@@ -30,27 +37,26 @@ void CSR::read_from_edgelist(ifstream& infile, string delimiter, bool pattern_on
         temp.erase(0, del_pos + del_size);
         
         del_pos = temp.find(delimiter);
-        std::string second_node_string = temp.substr(0, del_pos); //retrieve the part of the string after the delimiter
+        string second_node_string = temp.substr(0, del_pos); //retrieve the part of the string after the delimiter
         intT child = stoi(second_node_string);
         max_column = std::max(max_column, child);
-        temp.erase(0, del_pos + del_size);
-
-	if (!pattern_only) 
-	{
-		del_pos = temp.find(delimiter);
-        	std::string second_node_string = temp.substr(0, del_pos); //retrieve the part of the string after the delimiter
-        	DataT weigth = stof(second_node_string);
-	}
-
-
-	//if current_node != last_node:
-	//	save
-
+	    
+	del_pos = temp.find(delimiter);
+        string val_string = temp.substr(0, del_pos); //retrieve the part of the string after the delimiter
+        DataT val = stof(val_string);
+	    
         if (current_node > i)
         {
             while (i < current_node)
             {
-                i++;
+                vector<intT> new_pos_row;
+		pos_holder.push_back(new_pos_row);
+		if (not pattern_only)
+		{
+			vector<DataT> new_val_row;
+			val_holder.push_back(new_val_row);
+		}
+		i++;
             }
         }
         else if (current_node < i)
@@ -58,34 +64,38 @@ void CSR::read_from_edgelist(ifstream& infile, string delimiter, bool pattern_on
             std::cerr << "CANNOT READ MATRIX. INDICES MUST INCREASE" << std::endl;
             return 1;
         }
-        holder[i].push_back(child);
+        pos_holder[i].push_back(child);
+	val_holder[i].push_back(val);
+
     }
 
-    cmat.fmt = cmat_fmt;
-    intT main_dim = holder.size();
-    intT second_dim = max_column + 1;
-    cmat.rows = cmat_fmt ? second_dim : main_dim;
-    cmat.cols = cmat_fmt ? main_dim : second_dim;
-    cmat.nzcount = new intT[main_dim];
-    cmat.ja = new intT * [main_dim];
-    cmat.ma = new DataT * [main_dim];
+    rows = holder.size();
+    cols = max_column + 1;
+    nzcount = new intT[main_dim];
+    ja = new intT * [main_dim];
+    if (not pattern_only) ma = new DataT * [main_dim];
 
-    for (intT i = 0; i < holder.size(); i++)
+    for (intT i = 0; i < pos_holder.size(); i++)
     {
-        auto row = holder[i];
-        cmat.nzcount[i] = row.size();
-        cmat.ja[i] = new intT[row.size()];
-        cmat.ma[i] = new DataT[row.size()];
-
-        std::copy(row.begin(), row.end(), cmat.ja[i]);
-        std::vector<DataT> temp_vec(row.size(), 1.);
-        std::copy(temp_vec.begin(), temp_vec.end(), cmat.ma[i]); //entries = 1s. unweigthed
+        auto row_pos = pos_holder[i];
+        nzcount[i] = row_pos.size();
+        ja[i] = new intT[row_pos.size()];
+        std::copy(row_pos.begin(), row_pos.end(), ja[i]);
+	pos_holder[i].clear();
+	    
+	    
+	if (not pattern_only)
+	{
+	auto row_val = val_holder[i];
+	ma[i] = new DataT[row_val.size()];
+        std::copy(row_val.begin(), row_val.end(), ma[i]);
+	val_holder[i].clear();
+	}
     }
 
-    holder.clear();
+    pos_holder.clear();
+    val_holder.clear();
     return 0;
-}
-
 }
 
 void CSR::clean()
