fe427b82395edb672bfe979a8c8edf8c0163887b
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2022-07-21 16:41:37 +0200

    Update similarity.h

--- include/similarity.h
+++ include/similarity.h
@@ -1,1 +1,5 @@
+#include "matrices.h"
 
+float HammingDistance(intT* row_A, intT size_A, intT* row_B, intT size_B);
+float JaccardDistance(intT* row_A, intT size_A, intT* row_B, intT size_B);
+float CosineDistance(intT* row_A, intT size_A, intT* row_B, intT size_B);
