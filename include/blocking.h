17bd907e0c6b11df826f7816dc38d2f9112177b0
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2022-07-22 16:29:17 +0200

    Update blocking.h

--- include/blocking.h
+++ include/blocking.h
@@ -2,11 +2,15 @@
 #include <vector>
 #include "matrices.h"
 
+void reordering(const CSR& cmat, intT* grouping);
 
 
+/*
 class BlockingAlgorithm
 {
 	public string name;
 	public:
 		vector<long int> block(const CSR& cmat);	
 } algo;
+
+*/
