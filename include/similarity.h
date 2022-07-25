8b961e13e185c10eb9a34f503156de3d43a945e9
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2022-07-25 17:10:44 +0200

    fixed hamming similarity

--- include/similarity.h
+++ include/similarity.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "matrices.h"
 
-float HammingDistance(intT* row_A, intT size_A, intT* row_B, intT size_B);
+intT HammingDistance(intT* row_A, intT size_A, intT* row_B, intT size_B);
 float JaccardDistance(intT* row_A, intT size_A, intT* row_B, intT size_B);
 float CosineDistance(intT* row_A, intT size_A, intT* row_B, intT size_B);
