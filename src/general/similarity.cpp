b823dca8f6caad442f803bd9c9df511079c657a9
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2022-07-25 15:48:33 +0200

    Update similarity.cpp

--- src/general/similarity.cpp
+++ src/general/similarity.cpp
@@ -14,17 +14,17 @@ float HammingDistance(intT* row_A, intT size_A, intT* row_B, intT size_B)
   intT j = 0;
 
   cout << "NOT IMPLEMENTED YET" << endl;
-  return 0
+  return 0;
 }
 
 float JaccardDistance(intT* row_A, intT size_A, intT* row_B, intT size_B)
 {
   cout << "NOT IMPLEMENTED YET" << endl;
-  return 0
+  return 0;
 }
 
 float CosineDistance(intT* row_A, intT size_A, intT* row_B, intT size_B)
 {
   cout << "NOT IMPLEMENTED YET" << endl;
-  return 0
+  return 0;
 }
