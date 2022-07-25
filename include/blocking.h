9bd869e9096f3d1277a0476dfb616840ddc5f863
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2022-07-25 18:42:37 +0200

    Update blocking.h

--- include/blocking.h
+++ include/blocking.h
@@ -4,7 +4,7 @@
 #include "matrices.h"
 #include "similarity.h"
 
-typedef int (*distFunc)(intT*,intT,intT*,intT);
+typedef float (*distFunc)(intT*,intT,intT*,intT);
 
 void IterativeBlockingJaccard(const CSR& cmat, intT* grouping);
 void IterativeBlockingGeneral(const CSR& cmat, intT* grouping, float tau = 0.5, distFunc distanceFunction = &JaccardDistance);
