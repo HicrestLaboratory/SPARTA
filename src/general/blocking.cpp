ee4f9956d3d286b481c92dd9fe5a3c754316920d
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2022-07-25 17:46:09 +0200

    Update blocking.cpp

--- src/general/blocking.cpp
+++ src/general/blocking.cpp
@@ -19,6 +19,7 @@ void saad_reordering(const CSR& cmat, intT* grouping)
                 {
                     float dist = JaccardDistance(cmat.ja[i], cmat.nzcount[i], cmat.ja[j], cmat.nzcount[j]);
                     if (dist < tau) grouping[j] = i;
+                    cout << dist << endl;
                 }
             }
         }
