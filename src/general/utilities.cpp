75af14cc564ff5d11aff76632ccc60c48c51e589
paolosyloslabini <paolosyloslabini@gmail.com>
2022-12-16 11:14:28 +0100

    modified some files

--- src/general/utilities.cpp
+++ src/general/utilities.cpp
@@ -1,7 +1,28 @@
 #include <vector>
 #include "utilities.h"
 
+using namespace std;
+
+
 vector<intT> merge_rows(intT* A, intT sizeA, intT*B, intT sizeB)
 {
     //A,B sparse rows (compressed indices format)
+    intT i = 0;
+    intT j = 0;
+    vector<intT> result_vec = new vector<intT>();
+    while (i < sizeA && j < sizeB)
+    {
+        if (A[i] <= A[j])
+        {
+            i++;
+            result_vec.push_back(A[i]);
+        }   
+
+        if (A[i] > A[j])
+        {
+            j++;
+            result_vec.push_back(A[j]);
+        }
+    }
+    return result_vec;
 }
