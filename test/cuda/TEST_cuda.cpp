81f5a174cd99dcaaba92a466bf323714de5839b2
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2023-01-17 16:57:31 +0100

    Update TEST_cuda.cpp

--- test/cuda/TEST_cuda.cpp
+++ test/cuda/TEST_cuda.cpp
@@ -3,75 +3,85 @@
 #include "blocking.h"
 #include "utilities.h"
 
-//keep track of time
-float dt;
-vec_d algo_times;
-float mean_time;
-float std_time;
+using namespace std;
 
 
-//import CSR
-CLineReader cli(argc, argv);
-if (cli.verbose_ > 0) cli.print();
-CSR cmat_A(cli); //sparse operand
-BlockingEngine bEngine(cli);
 
-
-intT A_rows = cmat_A.rows;
-intT A_cols = cmat_A.cols;
-intT B_cols = A_rows;
-intT B_cols = 100;
-intT C_cols = A_cols;
-intT C_rows = B_rows;
-
-//dense operand
-DataT* mat_B = new DataT[B_rows * B_cols];
-
-//TODO initialize B with random numbers, not 1s
-for (intT i = 0; i < B_rows * B_cols; i++)
+int main(int argc, char* argv[])
 {
-    mat_B[i] = 1.;
+    //keep track of time
+    float dt;
+    vector<double> algo_times;
+    float mean_time;
+    float std_time;
+
+
+    //import CSR
+    CLineReader cli(argc, argv);
+    if (cli.verbose_ > 0) cli.print();
+    CSR cmat_A(cli); //sparse operand
+    BlockingEngine bEngine(cli);
+
+
+    intT A_rows = cmat_A.rows;
+    intT A_cols = cmat_A.cols;
+    intT B_cols = A_rows;
+    intT B_rows = 100;
+    intT C_cols = A_cols;
+    intT C_rows = B_rows;
+
+    //dense operand
+    DataT* mat_B = new DataT[B_rows * B_cols];
+
+    //TODO initialize B with random numbers, not 1s
+    for (intT i = 0; i < B_rows * B_cols; i++)
+    {
+        mat_B[i] = 1.;
+    }
+
+    //******************************************
+    //****Dense by dense MULTIPLICATION PHASE***
+    //******************************************
+
+
+    //TODO convert csr cmat_A to dense mat_A_gemm
+    DataT* mat_A_gemm = new DataT[A_rows * A_cols]{ 0 };
+
+    DataT_C* mat_Cgemm = new DataT_C[C_rows * C_cols]{ 0 }; //will store result of dense-dense multiplication
+
+    //run the dense-dense multiplications TODO
+
+    //for (int i = -cli.warmup_; i < cli.exp_repetitions_; i++)
+    //{
+    //    cublas_gemm_custom(mat_A_gemm, A_rows, A_cols, A_rows, mat_B, B_cols, B_rows, mat_Cgemm, C_rows, 1, 0, dt);
+    //    //only saves non-warmup runs
+    //    if (i >= 0) algo_times.push_back(dt);
+    //}
+
+    //mean_time = mean(algo_times);
+    //std_time = std_dev(algo_times);
+    //algo_times.clear();
+
+    //delete[] mat_A_gemm;
+
+
+
+    //******************************************
+    //****Sparse by dense MULTIPLICATION PHASE***
+    //******************************************
+
+
+    //TODO
+
+
+
+    //******************************************
+    //****VBR by dense MULTIPLICATION PHASE***
+    //******************************************
+
+
+    //TODO
+
+
+
 }
-
-//******************************************
-//****Dense by dense MULTIPLICATION PHASE***
-//******************************************
-
-
-//TODO convert csr cmat_A to dense mat_A_gemm
-DataT* mat_A_gemm = new DataT[A_rows * A_cols]{ 0 };
-
-DataT_C* mat_Cgemm = new DataT_C[C_rows * C_cols]{ 0 }; //will store result of dense-dense multiplication
-
-//run the dense-dense multiplications TODO
-
-//for (int i = -cli.warmup_; i < cli.exp_repetitions_; i++)
-//{
-//    cublas_gemm_custom(mat_A_gemm, A_rows, A_cols, A_rows, mat_B, B_cols, B_rows, mat_Cgemm, C_rows, 1, 0, dt);
-//    //only saves non-warmup runs
-//    if (i >= 0) algo_times.push_back(dt);
-//}
-
-//mean_time = mean(algo_times);
-//std_time = std_dev(algo_times);
-//algo_times.clear();
-
-//delete[] mat_A_gemm;
-
-
-
-//******************************************
-//****Sparse by dense MULTIPLICATION PHASE***
-//******************************************
-
-
-//TODO
-
-
-
-//******************************************
-//****VBR by dense MULTIPLICATION PHASE***
-//******************************************
-
-
-//TODO
