ad65c81c63ce871c084b450372c410b0856d80a9
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2023-01-20 13:20:41 +0100

    Update TEST_cuda.cpp

--- test/cuda/TEST_cuda.cpp
+++ test/cuda/TEST_cuda.cpp
@@ -33,7 +33,7 @@ int main(int argc, char* argv[])
     //create vbamt from equally spaced row partition of cmat_A;
     VBR vbmat; 
     if (cli.verbose_ > 0) cout << "Create VBR from reordered CSR" << endl;
-    vbmat2.fill_from_CSR(cmat, partition, cli.block_size_);
+    vbmat2.fill_from_CSR(cmat_A, partition, cli.block_size_);
     if (cli.verbose_ > 1) vbmat2.print();
 
 
