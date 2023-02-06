22012dfc5710fb239d1bf023335172ddebd48ea8
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2023-02-06 12:01:39 +0100

    removed some old data

--- src/scripts/utilities.py
+++ src/scripts/utilities.py
@@ -5,12 +5,8 @@ Created on Thu Aug  4 15:35:55 2022
 @author: Paolo
 """
 
-import csr as csr
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
-import itertools
-
 
 
 def evaluate_blocking(grouping,nz_block_count,block_width):
@@ -37,4 +33,29 @@ def evaluate_blocking(filename):
     #GROUPING,0,1,2,3,4,5,4,4,8,
 
     with open(filename) as infile:
+        complete = False
+        for idx, line in enumerate(infile):
+            line = line.split(",")[:-1]
+            if idx == 0: 
+                vars = line
+            elif idx == 1:
+                if len(line) != len(vars):
+                    print(f"ERROR: CSV ENTRIES MISMATCH IN {filename}")
+                    break
+                data = {var:value for var, value in zip(vars, line)}
+            elif idx == 2:
+                nzcount = line[1:]
+            elif idx == 3:
+                grouping = line[1:]
+                complete = True
+    if complete:
+        return data,nzcount,grouping
+    else:
+        print(f"ERROR: MISSING DATA in {filename}")
+        return {},[],[]
         
+
+def make_images(folder):
+
+
+print(evaluate_blocking("results/TEST_results.txt"))
