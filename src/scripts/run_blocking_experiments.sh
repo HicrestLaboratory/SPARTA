cebaad378d6385b2d75c85718d686fe3fb2c1714
paolosyloslabini <paolosyloslabini@gmail.com>
2023-02-06 12:20:44 +0100

    fixed experiment.sh with new option -a

--- src/scripts/run_blocking_experiments.sh
+++ src/scripts/run_blocking_experiments.sh
@@ -5,9 +5,12 @@ export PROGRAM=$3
 
 TAUs=(0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9)
 USE_PATTERN=(0 1)
-USE_STRUCTURED=(0 1)
-USE_GROUP=(0 1)
+USE_GROUP=(0)
 BLOCK_SIZEs=(32 64 128)
+ROW_BLOCK_SIZEs=(2 4 8 16 32 64 128 256 512 1024 2048 4096 8192)
+ALGOs=(0 1 2)
+r=1 #pre-reordering
+
 
 for fullpath in ${MATRICES_PATH}/*.el; do
 	MATRIX_FILE=$(basename -- "${fullpath}")
@@ -16,18 +19,25 @@ for fullpath in ${MATRICES_PATH}/*.el; do
 	MATRIX_FOLDER=${RESULTS_PATH}/${MATRIX_NAME}
 	mkdir ${MATRIX_FOLDER}
 	for b in ${BLOCK_SIZEs[@]}; do
-		for S in ${USE_STRUCTURED[@]}; do
-			for t in ${TAUs[@]}; do
-				for p in ${USE_PATTERN[@]}; do
-					for g in ${USE_GROUP[@]}; do
-						EXP_NAME="blocking_G_${MATRIX_NAME}_b_${b}_t_${t}_S_${S}_p_${p}_g_${g}"
-						OUTFILE=${MATRIX_FOLDER}/${EXP_NAME}.txt
-						./${PROGRAM} -f ${fullpath} -b ${b} -t ${t} -S ${S} -p ${p} -g ${g} -v 1 -o ${OUTFILE} -P 1 -n ${EXP_NAME}
+		for a in ${ALGOs[@]}; do
+			if [${a} -eq 2]
+			then
+				for B in ${ROW_BLOCK_SIZEs[@]};do
+					EXP_NAME="blocking_G_${MATRIX_NAME}_b_${b}_a_${a}_B_${B}_r_${r}"
+					OUTFILE=${MATRIX_FOLDER}/${EXP_NAME}.txt
+					./${PROGRAM} -f ${fullpath} -b ${b} -a ${a} -B ${B} -r ${r} -v 1 -o ${OUTFILE} -P 1 -n ${EXP_NAME}
+				done
+			else
+				for t in ${TAUs[@]}; do
+					for p in ${USE_PATTERN[@]}; do
+						for g in ${USE_GROUP[@]}; do
+							EXP_NAME="blocking_G_${MATRIX_NAME}_b_${b}_a_${a}_t_${t}_p_${p}_g_${g}_r_${r}"
+							OUTFILE=${MATRIX_FOLDER}/${EXP_NAME}.txt
+							./${PROGRAM} -f ${fullpath} -b ${b} -t ${t} -a ${a} -r ${r} -p ${p} -g ${g} -v 1 -o ${OUTFILE} -P 1 -n ${EXP_NAME}
+						done
 					done
 				done
-			done
+			fi
 		done
 	done
-
-
 done
