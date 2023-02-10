737c4e90cb16dcb015d6b7efe3278a4f9505405d
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2023-02-10 12:39:54 +0100

    test similrities

--- test/general/TEST_similarities.cpp
+++ test/general/TEST_similarities.cpp
@@ -11,27 +11,42 @@ int main(int argc, char* argv[])
     CLineReader cli(argc, argv);
     intT block_size = cli.col_block_size_;
 
-    vector<intT> row_A{1,2,5,10,12,20};
-    intT row_B[] = {0,2,4,10,16};
-    intT size_B = 5;
 
-    float hamming_sim = HammingDistanceGroup(row_A,1,row_B,size_B,1,block_size);
-    float jaccard_sim = JaccardDistanceGroup(row_A,1,row_B,size_B,1,block_size);
+    vector<vector<intT>> tests;
+    tests.push_back(vector<intT>{1,3}); 
+    tests.push_back(vector<intT>{0,5,10}); 
+    tests.push_back(vector<intT>{1,2,5,10,12,20}); 
+    tests.push_back(vector<intT>{0,3,7,11}); 
 
-    float hamming_sim_openmp = HammingDistanceGroupOPENMP(row_A,1,row_B,size_B,1,block_size);
-    float jaccard_sim_openmp = JaccardDistanceGroupOPENMP(row_A,1,row_B,size_B,1,block_size);
 
-    cout << "block_size: " << block_size << endl;
 
-    cout << "row A:  ";
-    print_vec(row_A);
+    
+    for (auto it = tests.begin(); it != tests.end(); it++)
+        for (auto it2 = tests.begin(); it2 != tests.end(); it2++)
+        {
+            cout << "*******************************************************************" << endl;
+            intT size_B = it2->size();
+            intT row_B[size_B];
+            copy(it2->begin(), it2->end(), row_B);
 
-    cout << "row B:  ";
-    print_mat(row_B, 1, size_B, 1);
+            float hamming_sim = HammingDistanceGroup(*it,1,row_B,size_B,1,block_size);
+            float jaccard_sim = JaccardDistanceGroup(*it,1,row_B,size_B,1,block_size);
 
-    cout << " Hamming: " << hamming_sim << endl;
-    cout << " Hamming(OPENMP): " << hamming_sim_openmp << endl;
- 
-    cout << " Jaccard: " << jaccard_sim << endl;
-    cout << " Jaccard(OPENMP): " << jaccard_sim_openmp << endl;
+            float hamming_sim_openmp = HammingDistanceGroupOPENMP(*it,1,row_B,size_B,1,block_size);
+            float jaccard_sim_openmp = JaccardDistanceGroupOPENMP(*it,1,row_B,size_B,1,block_size);
+
+            cout << "block_size: " << block_size << endl;
+
+            cout << "row A:  ";
+            print_vec(*it);
+
+            cout << "row B:  ";
+            print_mat(row_B, 1, size_B, 1);
+
+            cout << " Hamming: " << hamming_sim << endl;
+            cout << " Hamming(OPENMP): " << hamming_sim_openmp << endl;
+
+            cout << " Jaccard: " << jaccard_sim << endl;
+            cout << " Jaccard(OPENMP): " << jaccard_sim_openmp << endl;
+        }
 }
