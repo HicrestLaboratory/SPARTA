19a74f7dc53fb8f92497f5eb9b48c4f07c8bad23
paolosyloslabini <paolosyloslabini@gmail.com>
2024-07-17 00:20:36 +0200

    experiment collection

--- collect_results.sh
+++ collect_results.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
 # Paths (you can adjust these as needed)
-matrix_dir="../Clubs/matrices"
-result_dir="../../../Downloads/Club_result/Club_result"
-
+matrix_dir="../Clubs_tmp/matrices/toy"
+result_dir="../../../Downloads/Club_result/Club_result/toy"
+block_size=100
+metis_obj="edge-cut"
+metis_part=128
 
 # Loop through each matrix file in the matrix directory
-for matrix_file in "$matrix_dir"/*.mtx; do
+for matrix_file in $(find "$matrix_dir" -type f -name "*.mtx"); do
+    echo "************************************************"
+    echo " Processing file: $matrix_file"
     matrix_name=$(basename "$matrix_file" .mtx)
     
-    matrix_result_dir="$result_dir/$matrix_name"
-    if [! -d "$matrix_result_dir" ]; then
+    matrix_result_dir="$result_dir/${matrix_name}/GP/${metis_part}"
+    echo "Searching results in ${matrix_result_dir}"
+    if [ ! -d "$matrix_result_dir" ]; then
 	echo "Result folder not found for $matrix_name"
 	continue
-        # Loop through each technique directory within the result directory
-    technique_folder_dir=""
-        # Loop through each file in the technique directory
-        for result_file in "$technique_dir"/*; do
-            # Run the program with the given parameters
-            ./programs/general/Matrix_Analysis "$matrix_file" 105 "$result_file" 1
-        done
-    done
+    fi 
+    result_file="${matrix_result_dir}/${matrix_name}_metis_${metis_obj}_part${metis_part}.txt"
+    ./programs/general/Matrix_Analysis "$matrix_file" "$block_size" "$result_file" 1
 done
-
