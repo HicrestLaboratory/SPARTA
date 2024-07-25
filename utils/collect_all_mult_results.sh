45e0ad5a1ec47adba6c11ee130971a1ce660232a
paolosyloslabini <paolosyloslabini@gmail.com>
2024-07-25 12:59:37 +0200

    mult resuts better collect

--- utils/collect_all_mult_results.sh
+++ utils/collect_all_mult_results.sh
@@ -1,9 +1,57 @@
 #!/bin/bash
 
-# Define the root directory
-root_dir="../../../Downloads/mult_outputs"
-routine="spmvbsr"
+#************************************
+#*************  INPUT  **************
+#************************************************************************************************************************************************
+
+# Default values
+root_dir="../../../Downloads/mult_outputs_24"
+routine="spmmcsr"
+method="ALL" # Options: saad, metis, original, clubs, ALL
+
+# Function to display usage
+usage() {
+    echo "Usage: $0 [-r root_dir] [-u routine] [-m method]"
+    echo "  -r  Root directory (default: ../../../Downloads/mult_outputs_24)"
+    echo "  -u  Routine (default: spmvcsr)"
+    echo "  -m  Method (default: ALL)"
+    exit 1
+}
+
+# Parse options
+while getopts ":r:u:m:" opt; do
+    case ${opt} in
+        r )
+            root_dir=$OPTARG
+            ;;
+        u )
+            routine=$OPTARG
+            ;;
+        m )
+            method=$OPTARG
+            ;;
+        \? )
+            echo "Invalid option: -$OPTARG" 1>&2
+            usage
+            ;;
+        : )
+            echo "Invalid option: -$OPTARG requires an argument" 1>&2
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND -1))
+
+# Define directories based on the parsed or default values
 routine_dir="$root_dir/$routine/finished"
+routine_gp_dir="$root_dir/${routine}gp/finished"
+
+# Print the values to check
+echo "Root directory: $root_dir"
+echo "Routine: $routine"
+echo "Routine directory: $routine_dir"
+echo "Routine GP directory: $routine_gp_dir"
+echo "Method: $method"
 
 if [ ! -d "$routine_dir" ]; then
   echo "ERROR: COULD NOT FIND RESULT DIRECTORY $routine_dir"
@@ -13,6 +61,14 @@ fi
 # Output CSV file
 output_dir="results/results_2024/mult/$routine"
 mkdir -p "$output_dir"
+
+#************************************
+#************************************
+#************************************************************************************************************************************************
+
+#************************************************************************************************************************************************
+#************* UTILS    *************
+#************************************
 
 get_outfile() {
   local algo=$1
@@ -25,7 +81,10 @@ get_header() {
     clubs)
       echo "routine matrix algo mask centroid tau rows cols nnz time"
       ;;
-    metis)
+    metis-edge-cut)
+      echo "routine matrix algo objective parts rows cols nnz time"
+      ;;
+    metis-volume)
       echo "routine matrix algo objective parts rows cols nnz time"
       ;;
     saad)
@@ -38,6 +97,18 @@ get_header() {
       exit 1
       ;;
   esac
+}
+
+# Function to display progress bar
+show_progress() {
+    local current=$1
+    local total=$2
+    local progress=$((current * 100 / total))
+    local done=$((progress * 4 / 10))
+    local left=$((40 - done))
+    local fill=$(printf "%${done}s")
+    local empty=$(printf "%${left}s")
+    printf "\rProgress : [${fill// /#}${empty// /-}] $progress%%"
 }
 
 # Function to extract time from a file
@@ -84,8 +155,9 @@ extract_variables_saad() {
   collection=${collection#"$matrix-"}
   IFS='-' read -ra VARS <<< "$collection"
 
-  #TODO ADD TAU
-  echo "$matrix saad"
+  local tau=$(echo ${VARS[1]})
+
+  echo "$matrix saad $tau"
 }
 
 extract_variables_metis() {
@@ -93,12 +165,21 @@ extract_variables_metis() {
   IFS='_' read -ra PARTS <<< "$filename"
   local matrix=$( echo ${PARTS[1]} | sed "s/-mtx"// )
   local collection=${PARTS[2]}
-  collection=${collection#"$matrix-"}
+  collection=${collection#"$matrix-metis"}
+  local obj
+  local parts
   IFS='-' read -ra VARS <<< "$collection"
 
-  #TODO ADD PARAMS
+  if [[ $collection == *"volume"* ]]; then
+    obj="volume"
+    parts=$(echo ${VARS[2]} | sed 's/part//')
+  else
+    obj="edge-cut"
+    parts=$(echo ${VARS[3]} | sed 's/part//')
+  fi
 
-  collection=${collection#"$matrix-"}
+  #TODO ADD PARAMS
+  echo "$matrix metis $obj $parts"
 }
 
 # Function to extract variables from original output
@@ -117,7 +198,11 @@ find_file_type() {
   if [[ $file_name == *"msk"* && $file_name == *"cent"* && $file_name == *"tau"* ]]; then
     file_type="clubs"
   elif [[ $file_name == *"metis"* ]]; then # TODO CHECK
-    file_type="metis"
+    if [[ $file_name == *"edge"* ]];then
+      file_type="metis-edge-cut"
+    else
+      file_type="metis-volume"
+    fi
   elif [[ $file_name == *"saad"* ]]; then # TODO CHECK
     file_type="saad"
   else
@@ -132,7 +217,6 @@ process_file()
   local file_path=$1
 
   local file_name=$(basename "$file_path")
-  echo "Processing: $file_name"
 
   local time_vars=$(extract_time_and_variables "$file_path")
 
@@ -142,12 +226,18 @@ process_file()
   local nnz=$(echo "$time_vars" | awk '{print $4}')
 
   file_type=$(find_file_type "$file_name")
+  if [[ "$method" != "$file_type" && "$method" != "ALL" ]]; then
+     return 0
+  fi
+
   case $file_type in
     clubs)
-      echo "$(extract_variables_club "$file_name")"
       variables=$(extract_variables_club "$file_name")
       ;;
-    metis)
+    metis-edge-cut)
+      variables=$(extract_variables_metis "$file_name")
+      ;;
+    metis-volume)
       variables=$(extract_variables_metis "$file_name")
       ;;
     saad)
@@ -157,7 +247,8 @@ process_file()
       variables=$(extract_variables_original "$file_name")
       ;;
     *)
-      exit 1
+      echo "COULD NOT CLASSIFY $file_name"
+      return 1
       ;;
   esac
 
@@ -166,24 +257,57 @@ process_file()
   echo "$routine $variables $m $n $nnz $time" >> "$(get_outfile "$file_type")"
 }
 
-algos=( "clubs" "metis" "saad" "original" )
+
+#************************************
+#************************************
+#************************************************************************************************************************************************
+
+#************************************************************************************************************************************************
+
+#creates out files for processed methods
+if [[ "$method" == "ALL" ]]; then
+  algos=( "clubs" "metis-edge-cut" "metis-volume" "saad" "original" )
+elif [[ "$method" == "metis" ]]; then
+  algos=( "metis-edge-cut" "metis-volume")
+else
+  algos=( "$method" )
+fi
+
 for algo in "${algos[@]}"; do
   mkdir -p "$output_dir/$algo"
   echo "creating csv:" "$(get_outfile "$algo")"
   get_header "$algo" > "$(get_outfile "$algo")" # Print header for each algo
 done
+#-----------------------------------------
 
 
+
+#find files
 total_files=$(find "$routine_dir" -name '*.out' | wc -l)
-echo "Found files to process: $total_files"
-
+gp_files=$(find "$routine_gp_dir" -name '*.out' | wc -l)
+echo "Found files to process: $total_files + gp: $gp_files"
+#------------------------------------------------------
 
 counter=0
-# Iterate through each .out file in the method directory
-find "$routine_dir" -name '*.out' | grep "${routine}_" | while read -r file_path; do
-  process_file $file_path
-  counter=$((counter + 1))
-done
 
-echo "Processed $counter files out of $total_files"
+if [[ "$method" != "metis-edge-cut" && "$method" != "metis-volume" ]]; then
+    echo "Processing non-METIS files"
+  # Iterate through each .out file in the method directory
+  find "$routine_dir" -name '*.out' | grep "${routine}_" | while read -r file_path; do
+    process_file $file_path
+    counter=$(($counter + 1))
+    show_progress $counter $total_files
+  done
+fi
+
+if [[ "$method" == "metis-edge-cut" || "$method" == "metis-volume" || "$method" == "ALL" ]]; then
+  echo "Processing METIS files"
+  find "$routine_gp_dir" -name '*.out' | grep "${routine}" | while read -r file_path; do
+    process_file $file_path
+    counter=$(($counter + 1))
+    show_progress $counter $gp_files
+  done
+fi
+
+echo "Processed $counter files"
 echo "Results saved to $output_dir"
--- utils/make_mult_images.py
+++ utils/make_mult_images.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import gmean
 
-routine="spmvbsr"
-methods=["original", "clubs", "saad", "metis"]
+routine="spmmcsr"
+methods=["original", "clubs", "saad", "metis-edge-cut", "metis-volume"]
 
 # Define the directory containing the files
 directory = 'results/results_2024/mult/' + routine
@@ -17,11 +17,11 @@ columns={}
 columns["clubs"] = ["routine", "matrix", "algo", "mask", "centroid", "tau", "rows", "cols", "nnz", "time"]
 columns["original"] = ["routine", "matrix", "algo", "rows", "cols", "nnz", "time"]
 columns["saad"] = ["routine", "matrix", "algo", "tau", "rows", "cols", "nnz", "time"]
-columns["metis"] = ["routine", "matrix", "algo", "objective", "parts", "rows", "cols", "nnz", "time"]
+columns["metis-volume"] = ["routine", "matrix", "algo", "objective", "parts", "rows", "cols", "nnz", "time"]
+columns["metis-edge-cut"] = ["routine", "matrix", "algo", "objective", "parts", "rows", "cols", "nnz", "time"]
 
 dfs = {method: pd.DataFrame() for method in methods}
 files ={method: [os.path.join(directory + "/" + method + "/", f) for f in os.listdir(directory  + "/" + method + "/")] for method in methods}
-
 
 # Helper function to read and concatenate files, ignoring empty ones
 def read_and_concat(files):
@@ -57,14 +57,50 @@ def calculate_geometric_mean_ratios(dfs, methods):
         best_results = get_best_results(dfs[method],'matrix')
         merged = pd.merge(original_best[['matrix', 'time']], best_results[['matrix', 'time']], on='matrix', suffixes=('_original', '_method'))
         merged['ratio'] = merged['time_method'] / merged['time_original']
+        merged = merged[merged["ratio"] < 1] #only counts effective reorderings
         ratios[method] = gmean(merged['ratio'])
 
     return ratios
+
+# Function to count how many matrices each method is the best
+def count_best_methods(dfs, methods):
+    best_method_counts = {method: 0 for method in methods}
+    all_best_results = pd.DataFrame()
+
+    for method in methods:
+        if not dfs[method].empty:
+            best_results = get_best_results(dfs[method], ['matrix'])
+            best_results['method'] = method
+            all_best_results = pd.concat([all_best_results, best_results])
+
+    overall_best_results = get_best_results(all_best_results, ['matrix'], 'time')
+    
+    for method in methods:
+        best_method_counts[method] = (overall_best_results['method'] == method).sum()
+
+    return best_method_counts
+
+# Function to count unique matrices for each method
+def count_unique_matrices(dfs, methods):
+    unique_matrix_counts = {}
+    for method in methods:
+        if not dfs[method].empty:
+            unique_matrix_counts[method] = dfs[method]['matrix'].nunique()
+        else:
+            unique_matrix_counts[method] = 0
+    return unique_matrix_counts
 
 # Calculate geometric mean ratios
 geometric_mean_ratios = calculate_geometric_mean_ratios(dfs, methods)
 print(geometric_mean_ratios)
 
+# Count best methods
+best_method_counts = count_best_methods(dfs, methods)
+print(best_method_counts)
+
+# Count unique matrices for each method
+unique_matrix_counts = count_unique_matrices(dfs, methods)
+print("Unique matrix counts:", unique_matrix_counts)
 
 
 
