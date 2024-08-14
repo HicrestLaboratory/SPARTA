706e05ff3bdbc094296293ea895966d6b49db983
paolosyloslabini <paolosyloslabini@gmail.com>
2024-08-14 15:30:12 +0200

    Update make_mult_images_2.py

--- utils/make_mult_images_2.py
+++ utils/make_mult_images_2.py
@@ -5,7 +5,7 @@ import numpy as np
 from scipy.stats import gmean
 
 routine="spmvcsr"
-methods=["original", "clubs", "saad", "metis-edge-cut", "metis-volume", "denseAMP"]
+methods=["original", "clubs", "saad", "metis-edge-cut", "metis-volume"]
 
 # Define the directory containing the files
 root_dir="results/results_10-08-2024/"
@@ -47,10 +47,36 @@ for method in methods:
     allowed_matrices = len(dfs[method]["matrix"].unique())
     print(f"Method: {method}; Found data for {allowed_matrices} matrices")
 
+#find matrices common among ALL methods
+common_matrices=set(matrices_with_original)
+for method in methods:
+    if method != "original":
+        common_matrices &= set(dfs[method]['matrix'].unique())
+print(f"Found {len(common_matrices)} common matrices")
 
 def find_best(df):
     return df.loc[df.groupby("matrix")["time"].idxmin()]
     
-    
+best_dfs = {}
+for method in methods:
+    best_dfs[method] = find_best(dfs[method])
+
+def count_best_method():
+    all_results_df = pd.DataFrame()
+    for method in methods:
+        df = best_dfs[method]
+        df = df[df['matrix'].isin(common_matrices)]
+        all_results_df = pd.concat([all_results_df,df])
+
+    overall_best_results = find_best(all_results_df)
+    print(overall_best_results.columns)
+
+    best_method_counts = {}
+    for method in methods:
+        best_method_counts[method] = (overall_best_results['method'] == method).sum()
+
+count_best_method()
+
+
 #TRY: CALCULATE TOTAL INCLUDING RECTANGULAR FOR METIS
     
