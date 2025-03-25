9f3ecc4daa8e939fa22a889316f3e047025588a1
paolosyloslabini <paolosyloslabini@gmail.com>
2025-03-25 16:08:39 +0100

    Update collect_routine_mult_results.sh

--- utils/collect_routine_mult_results.sh
+++ utils/collect_routine_mult_results.sh
@@ -330,11 +330,13 @@ done
 
 for algo in "${algos[@]}"; do
     counter=0
+    echo "matching patterns: \n \t $root_dir/r${routine}*${algo}/finished/*.out \n \t $root_dir/$routine/$algo/finished/*.out"
     # Store the matching files in an array
     mapfile -t files < <(find "$root_dir" -type f \( \
           -path "$root_dir/$routine/$algo/finished/*.out" -o \
           -path "$root_dir/r${routine}*${algo}/finished/*.out" \
       \))
+
     
     total_files="${#files[@]}"
     echo "PROCESSING $total_files mult files from directories for $routine, $algo"
